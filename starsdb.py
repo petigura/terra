e0236b886594c3e3c6068f164974b4141475e26e
petigura <epetigura@berkeley.edu>
2010-11-29 09:15:35 -0800

    save progress

--- starsdb.py
+++ starsdb.py
@@ -1,8 +1,17 @@
+# To do Monday ...
+# Transfer over the star table function
+# Replace the class definitions with calls to star_table
+# See if the OID logic can be tightened (put in its own function)
+# Define an exoplanet object
+# Make sure the throughly test the pipeline 
+
 import matplotlib
 import numpy as np
 import re
 import os
-from elixir import *
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Column, Table, ForeignKey
+from sqlalchemy import Integer, String, Float
 
 import readstars,fxwid
 from matchstars import res2id
@@ -61,198 +70,156 @@ def readbensby06(file):
 
     return star,c_abund
 
-class Mystars(Entity):
-    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='mystars')
-    name = Field(Text)
-    oid  = Field(Text)
-    pop_flag = Field(Text)
-    d = Field(Integer)
-    vmag = Field(Float(precision=5))
-    logg = Field(Float(precision=5))
-    monh = Field(Float(precision=5))
+def star_table(tabname,metadata):
+    alchobj = Table(tabname,metadata,
+                    Column('id',Integer,primary_key=True),
 
-    vsini = Field(Float(precision=5))
-    teff = Field(Float(precision=5))
-
-    o_nfits = Field(Float(precision=5))
-    c_nfits = Field(Float(precision=5))
-
-    o_abund = Field(Float(precision=5))
-    c_abund = Field(Float(precision=5))
-
-    #abundances not corrected for temperature
-    o_abund_nt = Field(Float(precision=5)) 
-    c_abund_nt = Field(Float(precision=5))
-
-    fe_abund = Field(Float(precision=5))
-    ni_abund = Field(Float(precision=5))
+                    ###### Stellar Information ######
+                    Column('name',String(20)),
+                    Column('oid',Integer),
+                    Column('vsini',Float(precision=4) ),
+                    Column('teff',Integer),
+                    Column('pop_flag',String(10)),                    
+                    Column('vmag',Float(precision=4) ),
+                    Column('d',Float(precision=4) ),
+                    Column('logg',Float(precision=4) ),
+                    Column('monh',Float(precision=4) ),
 
 
-    o_staterrlo = Field(Float(precision=5))
-    o_staterrhi = Field(Float(precision=5))
+                    ###### VF05 Work ########
+                    Column('fe_abund',Float(precision=4) ),
+                    Column('ni_abund',Float(precision=4) ),
 
-    c_staterrlo = Field(Float(precision=5))
-    c_staterrhi = Field(Float(precision=5))
+                    ####### Fields Specific To Both Elements #######
 
-    o_scatterlo = Field(Float(precision=5))
-    o_scatterhi = Field(Float(precision=5))
+                    Column('o_abund_nt',Float(precision=4) ),
+                    Column('c_abund_nt',Float(precision=4) ),
 
-    c_scatterlo = Field(Float(precision=5))
-    c_scatterhi = Field(Float(precision=5))
+                    Column('o_abund',Float(precision=4) ),
+                    Column('c_abund',Float(precision=4) ),
 
-    o_nierrlo = Field(Float(precision=5))
-    o_nierrhi = Field(Float(precision=5))
+                    Column('o_staterrlo',Float(precision=4) ),
+                    Column('o_staterrhi',Float(precision=4) ),
 
+                    Column('c_staterrlo',Float(precision=4) ),
+                    Column('c_staterrhi',Float(precision=4) ),
 
-class Luckstars(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='luckstars')
+                    Column('o_scatterlo',Float(precision=4) ),
+                    Column('o_scatterhi',Float(precision=4) ),
 
-    name = Field(Text)
-    oid  = Field(Text)
-
-    o_abund = Field(Float(precision=5))
-    c_abund = Field(Float(precision=5))
-    c_staterr = Field(Float(precision=5))
-
-class Ramstars(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='ramstars')
-
-    name = Field(Text)
-    oid  = Field(Text)
-
-    teff = Field(Float(precision=5))
-    o_abund = Field(Float(precision=5))
-    o_err = Field(Float(precision=5))
-
-class Exo(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='exo')
-
-    name = Field(Text)
-    oid  = Field(Text)
-
-    msini = Field(Float(precision=5))
-    ecc = Field(Float(precision=5))
-    a = Field(Float(precision=5))
-    per = Field(Float(precision=5))
+                    Column('c_scatterlo',Float(precision=4) ),
+                    Column('c_scatterhi',Float(precision=4) ),
 
 
-class Ben04(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='ben04')
+                    ####### Fields Specific To Both Elements #######
 
-    name = Field(Text)
-    oid  = Field(Text)
-    o_abund = Field(Float(precision=5))
+                    Column('o_nierrlo',Float(precision=4) ),
+                    Column('o_nierrhi',Float(precision=4) ),
+                
+                    )
+    return alchobj
 
-class Ben06(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='ben06')
+def exo_table(tabname,metadata):
+    alchobj = Table(tabname,metadata,
+                    Column('id',Integer,primary_key=True),
 
-    name = Field(Text)
-    oid  = Field(Text)
-    c_abund = Field(Float(precision=5))
+                    Column('name',String(20)),
+                    Column('oid',Integer),
+                    Column('msini',Float(precision=4) ),
+                    Column('ecc',Float(precision=4) ),
+                    Column('a',Float(precision=4) ),
+                    Column('per',Float(precision=4) ),
+                    )
+    return alchobj
 
-class Red03(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='red03')
-
-    name = Field(Text)
-    oid  = Field(Text)
-    c_abund = Field(Float(precision=5))
-    o_abund = Field(Float(precision=5))
-    feh = Field(Float(precision=5))
-
-class Red06(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='red06')
-
-    name = Field(Text)
-    oid  = Field(Text)
-    c_abund = Field(Float(precision=5))
-    o_abund = Field(Float(precision=5))
-
-
-class Ben05(Entity):    
-    using_table_options(useexisting=True)
-    using_options(allowcoloverride=True,tablename='ben05')
-
-    name = Field(Text)
-    oid  = Field(Text)
-    c_abund = Field(Float(precision=5))
-    o_abund = Field(Float(precision=5))
-
-
+def fmtoid(idxarr,oidarr,i):
+    """
+    If the index i appears in the results then we include it in the sql table
+    """
+    if (idxarr == i).any():
+        return oidarr[np.where(idxarr == i)[0][0]]
+    else:
+        return None
 
 def mkdb():
 #    metadata.bind.echo = True
-    metadata.bind = "sqlite:///"+os.environ['STARSDB']
+    engine = create_engine("sqlite:///"+os.environ['STARSDB'],echo=True)
+    metadata = MetaData(bind=engine)
+    
 
     # if the file already exists destroy it
     if os.path.exists(os.environ['STARSDB']):
         os.system('rm '+os.environ['STARSDB'])
 
+    # Declare tables.
+    Luckstars = star_table('luckstars',metadata)
+    Ramstars =  star_table('ramstars',metadata)
+    Ben04 =  star_table('ben04',metadata)
+    Ben05 =  star_table('ben05',metadata)
+    Ben06 =  star_table('ben06',metadata)
+    Red03 =  star_table('red03',metadata)
+    Red06 =  star_table('red06',metadata)
+
+    # create tables in database
+    metadata.create_all()
+    conn = engine.connect()
 
     #### Add in mydata ####
     stars = readstars.ReadStars(os.environ['PYSTARS'])
     idxarr,oidarr = res2id('Comparison/myresults.sim')
 
-    setup_all(True)
+
+
+
     for i in range(len(stars.name)):        
-        if (idxarr == i).any():
-            oid = str(oidarr[np.where(idxarr == i)[0][0]])
-        else:
-            oid = None
+        ins = Mystars.insert()
+        oid = fmtoid(idxarr,oidarr,i)
 
         #ni abnd normalized to the sun
         ni_abund = stars.smeabund[i][27]-np.log10(stars.smeabund[i][0])+12.-6.17
 
-        Mystars(name=stars.name[i],
-                oid = oid,
+        star = ins.values(name=stars.name[i],
+                          oid = oid,
 
-                vsini = round(stars.vsini[i],3),
-                teff  = round(stars.teff[i],0),
-                pop_flag = stars.pop_flag[i],
-                vmag = round(stars.vmag[i],3),
-                d = 1/stars.prlx[i],
-                logg = round(stars.logg[i],3),
-                monh = round(stars.monh[i],3),
+                          vsini = round(stars.vsini[i],3),
+                          teff  = round(stars.teff[i],0),
+                          pop_flag = stars.pop_flag[i],
+                          vmag = round(stars.vmag[i],3),
+                          d = 1/stars.prlx[i],
+                          logg = round(stars.logg[i],3),
+                          monh = round(stars.monh[i],3),
 
 
-                o_nfits = float(stars.o_nfits[i]),
-                c_nfits = float(stars.c_nfits[i]),
+                          o_nfits = float(stars.o_nfits[i]),
+                          c_nfits = float(stars.c_nfits[i]),
+                          
+                          o_abund_nt = round(stars.o_abund[i],3),
+                          c_abund_nt = round(stars.c_abund[i],3),
+                          
+                          #place holders for the temperature corrected abundances
+                          o_abund = round(stars.o_abund[i],3),
+                          c_abund = round(stars.c_abund[i],3),
 
-                o_abund_nt = round(stars.o_abund[i],3),
-                c_abund_nt = round(stars.c_abund[i],3),
+                          fe_abund = round(stars.feh[i],3),
+                          ni_abund = round(ni_abund,3),
+                          
+                          o_staterrlo = round(stars.o_staterr[i,0],3),
+                          o_staterrhi = round(stars.o_staterr[i,1],3),
+                          
+                          c_staterrlo = round(stars.c_staterr[i,0],3),
+                          c_staterrhi =  round(stars.c_staterr[i,1],3),
 
-                #place holders for the temperature corrected abundances
-                o_abund = round(stars.o_abund[i],3),
-                c_abund = round(stars.c_abund[i],3),
-
-                fe_abund = round(stars.feh[i],3),
-                ni_abund = round(ni_abund,3),
-
-                o_staterrlo = round(stars.o_staterr[i,0],3),
-                o_staterrhi = round(stars.o_staterr[i,1],3),
+                          o_scatterlo = round(stars.o_scatter[i,0],3),
+                          o_scatterhi = round(stars.o_scatter[i,1],3),
+                          
+                          c_scatterlo = round(stars.c_scatter[i,0],3),
+                          c_scatterhi =  round(stars.c_scatter[i,1],3),
+                          
                 
-                c_staterrlo = round(stars.c_staterr[i,0],3),
-                c_staterrhi =  round(stars.c_staterr[i,1],3),
-
-                o_scatterlo = round(stars.o_scatter[i,0],3),
-                o_scatterhi = round(stars.o_scatter[i,1],3),
-                
-                c_scatterlo = round(stars.c_scatter[i,0],3),
-                c_scatterhi =  round(stars.c_scatter[i,1],3),
-
-                
-                o_nierrlo = round(stars.o_nierr[i,0],3),
-                o_nierrhi = round(stars.o_nierr[i,1],3),
-                )
-
+                          o_nierrlo = round(stars.o_nierr[i,0],3),
+                          o_nierrhi = round(stars.o_nierr[i,1],3),
+                          )
+        conn.execute(star)
 
     #### Add in Luck Data ####
     luckname,luckc505,luckc538,luckc659,luckc514,lucko = \
@@ -264,20 +231,19 @@ def mkdb():
     idxarr,oidarr = res2id('Comparison/Luck06/luckresults.sim')
 
     for i in range(len(luckname)):        
-        if (idxarr == i).any():
-            oid = str(oidarr[np.where(idxarr == i)[0][0]])
-        else:
-            oid = None
-            
+        oid = fmtoid(idxarr,oidarr,i)
+        ins = Luckstars.insert()
+
         carr = np.array([luckc505[i],luckc538[i],luckc659[i],luckc514[i]])
         cavg = np.mean(carr)
         cstd = np.std(carr)
-        Luckstars(name=luckname[i],
-                  oid = oid,
-                  o_abund = round(lucko[i],3),
-                  c_abund = round(cavg,3),
-                  c_staterr = round(cstd,3)
-                  )
+
+        star = ins.values(name=luckname[i],
+                          oid = oid,
+                          o_abund = round(lucko[i],3),
+                          c_abund = round(cavg,3),
+                          c_staterr = round(cstd,3)
+                          )
 
     #### Add in Exo Data ####
     idxarr,oidarr = res2id('Comparison/exoresults.sim')
