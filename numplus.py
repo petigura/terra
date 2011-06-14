3955ab269aed982ee3a9e14590876bb10749553e
Erik Petigura <epetigura@berkeley.edu>
2011-06-14 11:16:15 -0700

    Added hbinavg
    Rewrote binavg with a version that uses the built in numpy histogram
    function.  There are significant performance improvements for sorting
    into > 1000 bins.

--- numplus.py
+++ numplus.py
@@ -52,3 +52,45 @@ def binavg(x,y,bins):
 
     return binx,biny
 
+def hbinavg(x,y,bins):
+    """
+    Computes the average value of y on a bin by bin basis.
+
+    x - array of x values
+    y - array 
+    bins - array of bins in x
+
+    x = array([1,2,3,4])
+    y = array([1,3,2,4])
+    bins = array([0,2.5,5])
+    
+    returns
+    
+    array([1.25,3.75]),array([2.,3.])
+
+    Notes:
+    This function is simplier than previous binavg and uses built in
+    numpy functionality.  For situtations with few (<10) bins, binavg
+    performs better.  Above that, hbinavg blows it away.
+
+    ==== ======= ======= ======= ========
+    n    nbins   binavg  hbinavg speed up 
+    ==== ======= ======= ======= ========
+    4    2       48.8 us 236 us  0.2
+    10   10      111 us  236 us  0.5
+    1e3  1e3     8.16 ms 441 us  20
+    1e4  1e4     108 ms  2.55 ms 40
+    1e5  1e5     3.92 s  42.5 ms 100
+    1e3  10      147 us  280 us  0.5
+    1e3  1e2     8.04 ms 5.19 ms 1.5
+    1e6  1e5     32.3 s  304 ms  100
+    =====================================
+    """
+
+    binx = bins[:-1] + (bins[1:] - bins[:-1])/2.
+    bsum = ( np.histogram(x,bins=bins,weights=y) )[0]
+    bn   = ( np.histogram(x,bins=bins) )[0]
+    biny = bsum/bn
+
+    return binx,biny
+    
