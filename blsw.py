9cce1fb2d82666f4981134282d5df212b39308c6
Erik Petigura <epetigura@berkeley.edu>
2011-06-28 15:41:44 -0700

    Working C-implementation of a loop over phase and tdur

--- blsw.py
+++ blsw.py
@@ -7,8 +7,6 @@ from numpy import *
 from scipy import weave
 from scipy.weave import converters
 
-
-
 def blsmod(t,x,nf,fmin,df,nb,qmi,qma,n):
     """
     Python BLS - a modular version of BLS
@@ -49,7 +47,7 @@ def blsmod(t,x,nf,fmin,df,nb,qmi,qma,n):
 
 #------------------------------------------------------------------------
 #     turn n into a float
-    rn=float(n)
+    rn = float(n)
 
 # integer ceiling of the product
 # bin number of binned points in transit
@@ -112,7 +110,7 @@ def blsmod(t,x,nf,fmin,df,nb,qmi,qma,n):
             y[j] = y[j] + v[i]
             
 
-        power,jn1,jn2,rn3,s3 = loop2(y,ibi,kma,kmi,kkmi)
+        power,jn1,jn2,rn3,s3 = blsmod_loop2(y,ibi,kma,kmi,kkmi)
 
         power = sqrt(power)
         p[jf] = power
@@ -186,9 +184,223 @@ def blsmod_loop2(y,ibi,kma,kmi,kkmi):
 
     return power,jn1,jn2,rn3,s3
 
+def blsw(t,x,nf,fmin,df,nb,qmi,qma,n):
+    """
+    Python BLS - a modular version of BLS
+
+    Fits five parameters:
+       - P_0 - Fundemental period
+       - q   - fractional time of period in transit
+       - L   - low value
+       - H   - high value
+       - t_0 - epoch of transit       
+
+    The output is called signal residue
+    """
+    minbin = 5
+    nbmax = 2000
+
+    u = zeros(n)
+    v = zeros(n)
+
+    y   = zeros(nbmax)
+    ibi = zeros(nbmax)
+    p   = zeros(nf)
+
+    # Number of bins specified by the user cannot exceed hard-coded amount
+    if nb > nbmax:
+        print ' NB > NBMAX !!'
+        return None
+
+    # tot is the time baseline
+    tot = t[-1] - t[0]
+    # The minimum frequency must be greater than 1/T
+    # We require there be one dip.
+    # To nail down the period, wouldn't we want 2 dips.
+
+    if fmin < 1./tot:
+        print ' fmin < 1/T !!'
+        return None
+
+#------------------------------------------------------------------------
+#     turn n into a float
+    rn = float(n)
+
+# integer ceiling of the product
+# bin number of binned points in transit
+    kmi = int(qmi*float(nb))
+
+    if kmi < 1:
+        kmi=1
+
+#   maximum number of binned points in transit
+    kma = int(qma*float(nb)) + 1
+#   minimum number of (unbinned) points in transit
+    kkmi = int(rn*qmi)
+    if kkmi < minbin:
+        kkmi = minbin
+
+    bpow = 0.
+
+#=================================
+#     Set temporal time series
+#=================================
+
+    s = 0.
+
+#  Subtract start time and mean
+    
+    u = t-t[0]
+    v = x-np.mean(x)
+
+#==============================
+#     Triple-nested loop over
+#     1. Period
+#     2. Phase of transit
+#     3. Transit Duration
+#==============================
+
+    for jf in range(nf):
+        # jf - index for stepping through trial frequencies
+        f0 = fmin+df*float(jf) 
+        p0 = 1./f0 # p0 - period corresponding to the trial frequency
+
+#======================================================
+#     Compute folded time series with  *p0*  period
+#======================================================
+#     Zero-out working arrays
+        y = zeros(nbmax).astype(float)
+        ibi = zeros(nbmax).astype(int)
+
+#     Phase fold the data according to the trial period
+        for i in range(n):
+              
+# ph is the "phase" of the ith measurement (note their is a 2pi missing)
+            ph = u[i]*f0
+#     modulo(phase,period)
+            ph = ph-int(ph)
+#     j what bin is the ith data point in?
+            j  = int(nb*ph)
+#     ibi counts the number of points in the bin
+            ibi[j] = ibi[j] + 1
+#     y sums the points 
+            y[j] = y[j] + v[i]
+            
+
+        power,jn1,jn2,rn3,s3 = blsw_loop2(y,ibi,kma,kmi,kkmi)
+
+        power = sqrt(power)
+        p[jf] = power
+
+        if power > bpow:
+            bpow  =  power
+            in1   =  jn1
+            in2   =  jn2
+            qtran =  rn3/rn
+            depth = -s3*rn/(rn3*(rn-rn3))
+            bper  =  p0
+
+    return p,bper,bpow,depth,qtran,in1,in2
 
 
 
+
+
+
+
+
+def blsw_loop2(y,ibi,kma,kmi,kkmi):
+    """
+    This is the inner two nested for loops in the BLS algorithm
+
+    Given: 
+    y - an array of binned values
+    ibi - an array of the number of points in each bin.
+    
+    Return:
+    power - maximum signal residue for that period
+    jn1   - index of optimal ingress
+    jn2   - index of optimal egress
+    rn3   - number of unbinned points in the optimal transit
+    s     - sum of the points in the window
+
+    Example:
+    y   = [0,0,0,1,2,0,0,0,0,0]
+    ibi = [1,1,1,1,2,1,1,1,1,1]
+    should return
+    jn1 = 3
+    jn2 = 4
+    s   = 3
+    pow = 0.375
+    """
+
+    nb = len(y)
+    rn = float(np.sum(ibi))
+
+    code = """
+double s;
+double power = 0.0;
+
+int k;
+int kk;
+int nb2;
+
+double rn1;
+double pow;
+double rn3;
+int jn1;
+int jn2;
+double s3;
+
+py::tuple res(5);
+
+for (int i=0;i<nb;i++)
+  {
+    s = 0.0;
+    k = 0;
+    kk = 0;
+    nb2 = i+kma;
+    
+    if (nb2 >= nb)
+      {
+	nb2 = nb-1;
+      }
+    
+    for (int j=i;j<=nb2;j++)
+      {
+	k = k+1;
+	kk = kk + ibi(j);
+	s = s + y(j);
+
+	if ( (k > kmi) && (kk > kkmi) )
+	  {
+	    rn1 = (double) kk;
+	    pow = s*s/( rn1*(rn-rn1) );
+
+	    if (pow > power)
+	      {
+		power = pow;
+		jn1   = i;
+		jn2   = j;
+		rn3   = rn1;
+		s3    = s;
+	      }
+	  }
+      }
+
+  }
+
+res[0] = power;
+res[1] = jn1;
+res[2] = jn2;
+res[3] = rn3;
+res[4] = s3;
+
+return_val = res;
+"""
+    res = weave.inline(code,['y','ibi','kma','kmi','kkmi','rn','nb'],
+                       type_converters=converters.blitz)
+    return res
 
 
 
@@ -235,4 +447,69 @@ printf("%i",Nx[0]);
 """
     return weave.inline(code,['x'],type_converters=converters.blitz)
 
+
+def pos(x):
+    """
+    Simple test of conditional expressions in C
+
+    return 1 if positive else, 0.
+    """
+
+
+    code = """
+int pos;
+if (x < 0)
+ {
+   pos = 0;
+ }
+else
+ {
+   pos = 1;
+ } 
+   return_val = pos;
+"""
+    return weave.inline(code,['x'],type_converters=converters.blitz)
+
+
+
+def test_and(x,y):
+    """
+    Simple test of boolean operators in C
+
+    return 1 if x and y are positive
+    """
+    code = """
+int pos;
+if ( (x > 0) && (y > 0) )
+ {
+   pos = 1;
+ }
+else
+ {
+   pos = 0;
+ } 
+   return_val = pos;
+"""
+    return weave.inline(code,['x','y'],type_converters=converters.blitz)
+
+
+
+
+def f2i(x):
+    """
+    turn a float to an integer
+    """
+    code = """
+return_val = (int) x;
+"""
+    return weave.inline(code,['x'],type_converters=converters.blitz)
+
+def i2f(x):
+    """
+    turn an int to an float
+    """
+    code = """
+return_val = (double) x;
+"""
+    return weave.inline(code,['x'],type_converters=converters.blitz)
 
