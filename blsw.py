3e64cf2017df225b2e9c494d0d76ade590d1620f
Erik Petigura <epetigura@berkeley.edu>
2011-06-28 16:38:48 -0700

    Created a temporary function called blsw2.
    vectorized the creation of farr , the frequency array

--- blsw.py
+++ blsw.py
@@ -263,7 +263,134 @@ def blsw(t,x,nf,fmin,df,nb,qmi,qma,n):
     for jf in range(nf):
         # jf - index for stepping through trial frequencies
         f0 = fmin+df*float(jf) 
+        print f0
+
         p0 = 1./f0 # p0 - period corresponding to the trial frequency
+
+#======================================================
+#     Compute folded time series with  *p0*  period
+#======================================================
+#     Zero-out working arrays
+        y = zeros(nbmax).astype(float)
+        ibi = zeros(nbmax).astype(int)
+
+#     Phase fold the data according to the trial period
+        for i in range(n):
+              
+# ph is the "phase" of the ith measurement (note their is a 2pi missing)
+            ph = u[i]*f0
+#     modulo(phase,period)
+            ph = ph-int(ph)
+#     j what bin is the ith data point in?
+            j  = int(nb*ph)
+#     ibi counts the number of points in the bin
+            ibi[j] = ibi[j] + 1
+#     y sums the points 
+            y[j] = y[j] + v[i]
+            
+
+        power,jn1,jn2,rn3,s3 = blsw_loop2(y,ibi,kma,kmi,kkmi)
+
+        power = sqrt(power)
+        p[jf] = power
+
+        if power > bpow:
+            bpow  =  power
+            in1   =  jn1
+            in2   =  jn2
+            qtran =  rn3/rn
+            depth = -s3*rn/(rn3*(rn-rn3))
+            bper  =  p0
+
+    return p,bper,bpow,depth,qtran,in1,in2
+
+
+
+def blsw2(t,x,nf,fmin,df,nb,qmi,qma,n):
+    """
+    Python BLS - a modular version of BLS
+
+    Fits five parameters:
+       - P_0 - Fundemental period
+       - q   - fractional time of period in transit
+       - L   - low value
+       - H   - high value
+       - t_0 - epoch of transit       
+
+    The output is called signal residue
+    """
+    minbin = 5
+    nbmax = 2000
+
+    u = zeros(n)
+    v = zeros(n)
+
+    y   = zeros(nbmax)
+    ibi = zeros(nbmax)
+    p   = zeros(nf)
+
+    # Number of bins specified by the user cannot exceed hard-coded amount
+    if nb > nbmax:
+        print ' NB > NBMAX !!'
+        return None
+
+    # tot is the time baseline
+    tot = t[-1] - t[0]
+    # The minimum frequency must be greater than 1/T
+    # We require there be one dip.
+    # To nail down the period, wouldn't we want 2 dips.
+
+    if fmin < 1./tot:
+        print ' fmin < 1/T !!'
+        return None
+
+#------------------------------------------------------------------------
+#     turn n into a float
+    rn = float(n)
+
+# integer ceiling of the product
+# bin number of binned points in transit
+    kmi = int(qmi*float(nb))
+
+    if kmi < 1:
+        kmi=1
+
+#   maximum number of binned points in transit
+    kma = int(qma*float(nb)) + 1
+#   minimum number of (unbinned) points in transit
+    kkmi = int(rn*qmi)
+    if kkmi < minbin:
+        kkmi = minbin
+
+    bpow = 0.
+
+#=================================
+#     Set temporal time series
+#=================================
+
+    s = 0.
+
+#  Subtract start time and mean
+    
+    u = t-t[0]
+    v = x-np.mean(x)
+
+#==============================
+#     Triple-nested loop over
+#     1. Period
+#     2. Phase of transit
+#     3. Transit Duration
+#==============================
+
+    farr = np.linspace(fmin,fmin+nf*df,nf) 
+    
+    for jf in range(nf):
+        # jf - index for stepping through trial frequencies
+        f0 = farr[jf]
+        print f0
+        p0 = 1./f0 # p0 - period corresponding to the trial frequency
+
+
 
 #======================================================
 #     Compute folded time series with  *p0*  period
