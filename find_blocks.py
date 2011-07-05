b35fe5e98a537e1dc65a795fcfee6ac21bbd1ec4
Erik Petigura <epetigura@berkeley.edu>
2011-07-05 15:32:33 -0700

    Working verision of Bayesian Blocks for point measurements
    For a ~4000 point time series this code took about 10min to run.

--- find_blocks.py
+++ find_blocks.py
@@ -23,8 +23,7 @@ def evt(tt):
     mdpt = 0.5*(tt[:-1] + tt[1:]) # pts btw ticks
 
     # Time from tick i to the last tick
-    t2end = tt_top - append(tt_bot,mdpt)
-    
+    t2end = tt_top - append(tt_bot,mdpt)    
     best,last = array([]),array([]).astype(int)
 
     for r in range(0,n):
@@ -51,3 +50,59 @@ def evt(tt):
         best,last = append( best,lpart[rstar] ),append( last,rstar )
 
     return last
+
+
+def pt(t,x,sig):
+    """
+    Bayesian blocks algorithm for point measurements.  
+
+    Use the maximum likelihold as a measure of block fitness.
+    """
+
+    # Ensure data are sequential in time.
+    sidx = argsort(t)
+    t   = t[sidx]
+    x   = x[sidx]
+    sig = sig[sidx]
+
+    n = len(t)
+
+    best,last,val = array([]),array([]).astype(int),array([])
+
+    for r in range(n):
+
+        cells = arange(r+1)
+
+        o = [ pt_bfit(x[c:r+1],sig[c:r+1]) for c in cells]
+        o = array(o) 
+        Lend,valend = o[::,0],o[::,1]
+
+        Ltot = append(0,best) + Lend - 10
+
+        # r* the change point that maximizes fitness of partition
+        rstar = argmax( Ltot)
+        best  = append( best,Ltot[rstar] )
+        last  = append( last,rstar )
+        val   = append( val,valend[rstar] )
+
+        if mod(r,100) ==0:
+            print r
+
+    return last,val
+
+def pt_bfit(x,sig):
+    """
+    Given a data block return it's maximum likelihood.
+    """
+
+    sig2 = sig**2
+
+    a = 0.5*sum(1. / sig2)
+    b = -1.0*sum(x / sig2)
+    c = 0.5*sum(x**2/sig2)
+
+    maxl = b**2 / (4*a) - c
+    maxval = -b / (2*a)
+
+    return maxl,maxval
+    
