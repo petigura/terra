5a28b039e57e040339072fee339c152fc52f2a73
Erik Petigura <epetigura@berkeley.edu>
2011-07-05 17:51:47 -0700

    Implemented a C version of likelihood calculator.
    Speed up is ~1000x !
    
    This commit has both the pure python and C verions

--- find_blocks.py
+++ find_blocks.py
@@ -3,6 +3,9 @@ Python implemenation of BB.
 """
 
 from numpy import *
+from scipy import weave
+from scipy.weave import converters
+
 
 def evt(tt):
     """
@@ -70,7 +73,7 @@ def pt(t,x,sig):
     best,last,val = array([]),array([]).astype(int),array([])
 
     for r in range(n):
-        Lend,valend = pt_last2(x[:r+1],sig[:r+1])
+        Lend,valend = pt_lastw(x[:r+1],sig[:r+1])
         Ltot = append(0,best) + Lend - 10
 
         # r* the change point that maximizes fitness of partition
@@ -79,40 +82,8 @@ def pt(t,x,sig):
         last  = append( last,rstar )
         val   = append( val,valend[rstar] )
 
-        if mod(r,100) ==0:
-            print r
 
     return last,val
-
-def pt_last(x,sig,r):
-    """
-    Return the likelihood and mostlikely values for block ending at r
-    """
-
-    cells = arange(r+1)
-
-    o = [ pt_bfit(x[c:r+1],sig[c:r+1]) for c in cells]
-    o = array(o) 
-    Lend,valend = o[::,0],o[::,1]
-
-    return Lend,valend
-
-def pt_bfit(x,sig):
-    """
-    Given a data block return it's maximum likelihood.
-    """
-
-    sig2 = sig**2
-
-    a = 0.5*sum(1. / sig2)
-    b = -1.0*sum(x / sig2)
-    c = 0.5*sum(x**2/sig2)
-
-    maxl = b**2 / (4*a) - c
-    maxval = -b / (2*a)
-
-    return maxl,maxval
-    
 
 def pt_last2(x,sig):
     """
@@ -123,9 +94,7 @@ def pt_last2(x,sig):
     a = 0.5*sum(1. / sig2)
     b = -1.0*sum(x / sig2)
     c = 0.5*sum(x**2/sig2)
-
     """
-
 
     sig2 = sig**2
     x2 = x**2
@@ -140,8 +109,10 @@ def pt_last2(x,sig):
         b -= x[r] / sig2[r]
         c += 0.5 * x2[r] / sig2[r]
 
-        maxl   = append(maxl,   b**2 / (4*a) - c )
-        maxval = append(maxval, -b / (2*a) )
+        l = b**2 / (4*a) - c
+        val =  -b / (2*a)
+        maxl   = append(maxl,  l   )
+        maxval = append(maxval,val )
 
     # Arrays are filled backward, so reverse them.
     maxl   = maxl[::-1]
@@ -149,6 +120,32 @@ def pt_last2(x,sig):
 
     return maxl,maxval
     
+def pt_lastw(xx,sig):
+    """
+    Given a data block return it's maximum likelihood.
+
+    Try to get rid of redundant summing
+
+    a = 0.5*sum(1. / sig2)
+    b = -1.0*sum(x / sig2)
+    c = 0.5*sum(x**2/sig2)
+
+    """
+
+
+    n = len(xx)
+    maxl = zeros(n).astype(float)
+    maxval = zeros(n).astype(float)
+    
+    # TODO: opening the file inside the loop is inefficient
+    fid = open('ccode/pt_loop.c') 
+    code = fid.read()
+    fid.close()
+
+    weave.inline(code,['xx','sig','n','maxl','maxval'],
+                 type_converters=converters.blitz)
+
+    return maxl,maxval
 
 
 
