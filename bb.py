6b5d719cdce56de9bdf50c92554d82f8b3405d31
Erik Petigura <epetigura@berkeley.edu>
2012-01-24 10:03:00 -0800

    Got rid of some functions I don't use anymore.
    tf
    nQ
    gaps
    overlap
    igeg
    complete
    dtrunmed
    runmed
    splitq
    qdt
    designL
    designLS
    fmodel
    mqclipPK
    sQ

---
+++ bb.py
@@ -0,0 +1,29 @@
+"""
+Bayesian Blocks Transit Finder
+"""
+
+from numpy import *
+
+def get_blocks(x_inp,last,val):
+    """
+    Just return the blocks.
+
+    If a timeseries only has one block, all elements will be equal.
+    """
+
+    # Don't modify the input argument
+    x = copy(x_inp)
+
+    cp = unique(last)
+    ncp  = len(cp)
+
+    n = len(x)
+    idxlo = cp                    # index of left side of region
+    idxhi = append(cp[1:],n)-1 # index of right side of region 
+
+    for i in range(ncp):
+        x[ idxlo[i]:idxhi[i]+1 ] = val[ idxhi[i] ]
+
+    return x
+
+
--- detrend.py
+++ detrend.py
@@ -10,6 +10,7 @@ from numpy.polynomial import Legendre
 from keptoy import * 
 from scipy import ndimage as nd
 from scipy.interpolate import UnivariateSpline
+from scipy.optimize import fmin 
 
 # Load up multiquarter data.
 
@@ -18,169 +19,7 @@ dmi = 1.    # remove strips of data that are less than 1 day long.
 gmi = 1/24. # Let's not worry about gaps that are 1 hour long
 nq = 8
 kepdir = os.environ['KEPDIR']
-
-def tf(tset):
-    time,flux = ma.masked_array([]),ma.masked_array([])
-
-    for i in range(nq):
-        tq = ma.masked_array( tset[i].TIME )
-        fq = ma.masked_array( tset[i].SAP_FLUX )
-
-        time = ma.concatenate((time,tq))
-        flux = ma.concatenate((flux,fq))
-
-    return time,flux
-
-def nQ(tset):    
-    ntab = 0
-    for t in tset:
-        ntab +=1
-
-    return ntab
-
-def gaps(tset):
-    """
-    This function identifies gaps in the data signified either by:
-    - nan values for time
-    - inter quarter separtions
-
-    returns two arrays with the start and end times of each gap.
-    """
-    g0,g1 = array([]),array([])
-
-
-    ntab = nQ(tset)
-
-    for i in range(ntab):
-        tq = ma.masked_array( tset[i].TIME )
-        fq = ma.masked_array( tset[i].SAP_FLUX )
-        tq = ma.masked_invalid( tq ) 
-        sl = ma.notmasked_contiguous(tq)
-
-        for s in sl:
-            g0 = append(g0, tq[s.stop] )
-            g1 = append(g1, tq[s.start] )
-    # Merge gaps that are not separated by very much data.
-
-    id = where(g0-g1 > dmi)[0]
-    g0,g1 = g0[id],g1[id]
-
-    # Since the earliest gap marker, is an end, our idicies are off by one
-    g0,g1 = g0[:-1],g1[1:]
-
-    # Ignore gaps that are very short (several cadences).
-    id = where(g1-g0 > gmi)[0]   
-    g0,g1 = g0[id],g1[id]
-    return g0,g1
-
-def overlap(t0,t1,g0,g1):
-    """
-    Given a list of prospective transit times, we can see whether they
-    overlap with the gaps.  If they do, we reject that point.  
-
-    In the case of 3 or 4 gaps we would want to see every single one.
-    If there were more transits ~10 we could get away with missing a few.
-    """
-
-    fid = open('ccode/tgap.c')
-    code = fid.read()
-    fid.close()
-
-    nt = len(t0)
-    ng = len(g0)
-
-    ov = weave.inline(code,['t0','t1','g0','g1','nt','ng'],
-                 type_converters=converters.blitz)
-    return ov
-
-def igeg(ig1,P,nt,tdur):
-    """
-    Return arrays specifying ingress and egress times for a specified
-    epoch, period, number of transits, and transit duration.
-    """
-
-    ig = ig1 + arange(nt)*P
-    eg = ig + tdur
-    return ig,eg
-
-def complete():
-    tma  = max(time)
-    tmi  = min(time)
-    tbase = tma - tmi
-    
-    # This is the maximum period we could have and still possibly see
-    # three transits
-
-    Pma  = tbase / 2.  
-    nP = 500.
-    Parr = logspace( log10(100),log10(Pma),nP )     
-    eff = zeros(nP)
-
-    for i in range(nP):
-        P = Parr[i]
-        tdur = a2tdur(P2a(P))
-        nph = P / tdur
-        ig1arr = tmi + linspace(0,P,nph)
-
-        # The maximum number of transits that can occur with a specified period
-        nt = ceil( tbase / P )
-        ocount = 0 
-
-        for ig1 in ig1arr:
-            ig,eg  = igeg(ig1,P,nt,tdur)
-            ocount += overlap(ig,eg,g0,g1)
-            
-        eff[i] = 1 - ocount/nph
-
-    return Parr,eff
-
-
-def dtrunmed(timel,fluxl):
-    """
-        
-    """
-    fluxl,timel = stitch(fluxl,timel)
-    ftot,ttot = array([]),array([])
-
-    nseg = len(timel)
-    assert len(timel) == len(fluxl)
-
-    for i in range(nseg):
-        ftot = append(ftot,fluxl[i] )
-        ttot = append(ttot,timel[i] )
-
-    tdt,fdt,fint = array([]),array([]),array([])
-
-    for i in range(nseg):
-        time,flux = timel[i],fluxl[i]
-        fl,tl = splitq(flux,time)
-        nseg = len(fl)
-
-        for j in range(nseg):       
-            x,y = runmed(fl[j],tl[j])
-            yint = interp1d(x,y,bounds_error=False)
-            
-            tdt = append(tdt,time)
-            fdt = append(fdt,flux-yint(time))
-            fint = append(fint,yint(time) )
-
-    tdt = ma.masked_invalid(tdt).astype(float32)
-    fdt = ma.masked_invalid(fdt).astype(float32)
-    fint = ma.masked_invalid(fint).astype(float32)
-
-
-    mask = tdt.mask | fdt.mask
-    tdt.mask = mask
-    fdt.mask = mask
-    fint.mask = mask
-
-
-    tdt = tdt.compressed()
-    fdt = fdt.compressed()
-    fint = fint.compressed()
-
-    return tdt,fdt,fint
-
+cbvdir = os.path.join(kepdir,'kepdat/CBV/')
 
 def stitch(fl,tl,swd = 0.5):
     """
@@ -204,192 +43,7 @@ def stitch(fl,tl,swd = 0.5):
 
     return fl,tl
 
-def runmed(flux,time):
-    mbpts = isnan(flux) 
-
-    flux = ma.masked_array( flux)
-    time = ma.masked_array( time,mask=isnan(flux) )
-    
-    twid = 2
-    cad = 30./60./24.
-    npts = len(time)
-
-    x = []
-    y = []
-
-    tmid = time.min() + twid / 2.
-    tmidlast = time.max() - twid / 2.
-
-    while tmid < tmidlast:
-        tm = ma.getmask( ma.masked_outside(time,tmid-twid/2.,tmid + twid/2.) )
-        reg = ma.masked_array(flux,mask=ma.mask_or(mbpts,tm) )
-
-        x.append( tmid )
-        y.append( ma.median( reg ) )
-        tmid += cad
-
-    x = array(x)
-    y = array(y)
-    
-    return x,y
-
-
-# TODO: Use Masked arrays so that tout and fout are the same shape as the input tables
-def splitq(flux,time):
-    # split quarters if the distance between good points is too big.
-    mgap = 0.5 * 48
-
-    mbpts = isnan(flux) 
-    bflux = ma.masked_array(flux,mask=~mbpts)
-    sl = array( ma.notmasked_contiguous(bflux) )
-    if len(sl) == 0:
-        return [flux],[time]
-
-    gapl = array([s.stop - s.start for s in sl])
-
-    fl = []
-    tl = []
-
-    left = 0
-
-    splitid = where( gapl > mgap )[0]
-    nseg = len(splitid) + 1
-    if nseg > 1:
-        for id in splitid:
-            right = sl[id].start - 1
-            fl.append(flux[left:right])
-            tl.append(time[left:right])
-
-            # increment left edge
-            left = sl[id].stop + 1
-
-        fl.append(flux[left:])
-        tl.append(time[left:])
-
-        return fl,tl
-    else:
-        return [flux],[time]
-
 from scipy.interpolate import interp1d
-
-def qdt(time,flux):
-    """
-    Detrend a Quarter 
-    """
-    fl,tl = splitq(flux,time)
-    nseg = len(fl)
-
-    fout,tout = array([]),array([])
-
-    for j in range(nseg):        
-        x,y = runmed(fl[j],tl[j])
-        yint = interp1d(x,y,bounds_error=False)
-        
-        fout = append(fout, fl[j] - yint( tl[j] ) )
-        tout = append(tout, tl[j])
-    return tout,fout 
-
-
-# Construct the design matrix
-
-def designL(x,order=3,domain=[-1,1]):
-    """
-    
-    """
-    nData  = len(x)
-    nParam = order + 1
-    X = zeros((nData,nParam))
-
-    for i in range(nParam):
-        coeff = zeros(nParam)
-        coeff[i] = 1
-        L = Legendre(coeff,domain=domain)
-        X[::,i] = L(x)
-    return X
-
-
-
-def designLS(t,f,p,order=3,domain=[-1,1]):
-    """
-    A lightcurve is fit to the following function:
-    f = [b] * Legendre( [coeff] )(t)
-    
-    Since the parameters are not linear, we'll linearize the problem.
-
-    df/d[p] * delta [p] =  y_data - y_0
-
-    We use linear least squares fitting to solve for the differences.  This
-    function computes the design matrix df/d[p].
-
-    Inputs:
-
-    t - Times where these points were taken.
-    f - Array of fluxes shape = (nLightCurves, nDataPoints)   
-    p - Array of parameters.  Must be in the following order:
-        [ b_0 ... b_{nLC - 1} , a_1 ... a_{order} ]
-
-        bs are the multiplicative weights given to each LC
-        as are the Legendre coeff we do not fit for the constant term.
-    """
-    
-    nLC   = f.shape[0]
-    nData = f.shape[1]
-    # Require that arrays are 1-D
-    p = p.reshape(p.size)
-
-    nb = nLC - 1
-    assert len(p) == order + nb
-
-    # nLC*nData by nLC+order+1
-    X = zeros( (nb*nData,nb+order) )
-
-    a = p[nb:]
-    b = p[:nb]
-    coef = hstack( (0,a) )
-
-    shapeFunc = Legendre(coef,domain=domain)(t)
-    for iLC in range(nb):
-        sL = iLC*nData     # Low slice.  Index of the first row of D-matrix.
-        sH = (iLC+1)*nData # High slice. Index of the last row of D-matrix + 1.
-
-        X[sL:sH,iLC] =  shapeFunc
-
-        for j in range(order):
-            iOrder = j+1
-            dCol = nb+j
-            bcoef = zeros(order+1) # Coefficient for the Legendre basis functions
-
-            bcoef[iOrder] = coef[iOrder]
-
-            X[sL:sH,dCol] = b[iLC]*Legendre(bcoef,domain=domain)(t)
-        
-    return X
-
-    
-def fModel(p,t,f,domain=[-1,1],order=3):
-    """
-    p - Array of parameters.  Must be in the following order:
-    """
-
-    # Require that arrays are 1-D
-    p = p.reshape(p.size)
-    nLC   = f.shape[0]
-    nData = f.shape[1]
-
-    nb = nLC - 1
-    assert len(p) == order  + nb
-
-    b = p[:nb]
-    a = p[nb:]
-    coef = hstack( (0,a) )
-
-    fM = zeros(f.shape)
-    fM[0,::] = Legendre(coef,domain=domain)(t)
-    for i in range(nb):
-        iLC = i + 1
-        fM[iLC,::] = b[i]*Legendre(coef,domain=domain)(t)
-        
-    return fM
 
 
 def cbvDT(KIC):
@@ -445,23 +99,6 @@ def larr(iL):
         
     return oL
 
-def mqclipPK(infiles):
-    """
-    Wrapper around the PyKE routines.
-    """
-    rfile= os.path.join(kepdir,'ranges/cut_bjd.txt')
-
-    from pyraf import iraf
-    iraf.kepler(_doprint=0)
-    for f in infiles:
-        bname = os.path.basename(f)
-        outf = os.path.join(kepdir,'tempfits/clip/',bname)        
-        
-        iraf.kepler.kepclip(infile=f,outfile=outf,ranges='@'+rfile,
-                            clobber=True,plot=False,verbose=False,plotcol='SAP_FLUX')
-
-
-
 
 def mqclip(t,f):
     rfile= os.path.join(kepdir,'ranges/cut_time.txt')
@@ -504,47 +141,88 @@ def nanIntrp(x0,y0,nContig=3):
     return x.data,y.data
 
 
-def sQ(tset,swd = 0.5):
+
+def cbv(tQLC,fcol,efcol,mask=None):
     """
-    Stitch together the boundaries of the quarters.
+    Cotrending basis vectors.
+
+    My implimentation of CBV detrending.  Assumes the relavent
+    lightcurve has been detrended.
+
+    Paramaters
+    ----------
+
+    tQLC    : Table for single quarter.
+    fcol    : string.  name of the flux column
+    efol : string.  name of the flux_err colunm
+
+    Returns
+    -------
+
+    ffit    : The CBV fit to the fluxes.
     """
-    swd =  0.5 # How long a timeseries on either end to use days 
-    Ql = [t.keywords['QUARTER'] for t in tset]
-    
-    assert (sort(Ql) == Ql).all(), "Quarters are not in order" 
-    nQ = len(Ql)
+    cbv = [1,2,3,4,5,6,7,8]
 
-    for i in range(nQ-1):
-        qleft,qright = tset[i],tset[i+1]
+    kw = tQLC.keywords
+    assert kw['nQ'],'Assumes lightcurve has been normalized.' 
 
-        fleft = qleft.SAP_FLUX
-        tleft = qleft.TIME
+    # Load up the BV
+    bvfile = os.path.join( cbvdir,'kplr*-q%02d-*.fits' % kw['QUARTER'])
+    bvfile = glob.glob(bvfile)[0]
+    bvhdu  = pyfits.open(bvfile)
+    bvkw   = bvhdu[0].header
+    bvcolname = 'MODOUT_%i_%i' % (kw['MODULE'],kw['OUTPUT'])
+    tBV    = atpy.Table(bvfile,hdu=bvcolname,type='fits')
+    assert bvkw['QUARTER'] == kw['QUARTER']," BV must be from the same quater"
 
-        fright = qright.SAP_FLUX
-        tright = qright.TIME
+    cad   = tQLC['CADENCENO' ]
+    t     = tQLC['TIME'      ]
+    f     = tQLC[fcol        ]
+    ferr  = tQLC[efcol       ]
 
-        # Mask out points away from the edge.
-
-        tleft = ma.masked_less(tleft,tleft[-1] - swd) 
-        tright = ma.masked_greater(tright,tright[0] + swd) 
-
-        # Mask out nan points
-
-        fleft = ma.masked_invalid(fleft)
-        fright = ma.masked_invalid(fright)
-
-        mleft = fleft.mask | tleft.mask
-        mright = fright.mask | tright.mask
-        fleft.mask = mleft
-        fright.mask = mright
-
-        assert (fleft.count() > 10) & (fright.count() > 10) , "not enought points to fit a line"
-
-        offset = ma.median(fright) - ma.median(fleft)
-        tset[i+1].SAP_FLUX -= offset
+    if mask is not None:
+        assert mask.size == cad.size, "Arrays must be of equal sizes."
+        mid = where(~mask)[0]
+        cad   = cad[mid]
+        t     = t[mid]
+        f     = f[mid]
+        ferr  = ferr[mid]
 
 
-    return tset
+    tm = ma.masked_invalid(t)
+    fm = ma.masked_invalid(f)
+    mask  = tm.mask | fm.mask 
 
+    gid   = where(~mask)[0]
+    bid   = where(mask)[0]
 
+    t     = t[gid]
+    f     = f[gid]
+    cad   = cad[gid] 
+    ferr  = ferr[gid] 
 
+    mbv = lambda i: tBV['VECTOR_%i' % i][gid]
+    bvectors = map(mbv,cbv)
+    bvectors = vstack(bvectors)
+
+    p0 = np.zeros( len(cbv) ) # Guess for parameters.
+    p0[:2] = 1
+
+    p1 , fopt ,iter ,funcalls, warnflag = \
+        fmin(objCBV,p0,args=(f,ferr,bvectors) ,disp=False,full_output=True)
+
+    ffit      = fm.data.copy()
+    ffit[gid] = modelCBV(p1,bvectors).astype(float32)
+    ffit[bid] = np.nan    
+    return ffit
+
+def objCBV(p,f,ferr,bvectors):
+   """
+   Objective function for CBV vectors.
+   """
+   nres = ( f - modelCBV(p,bvectors) ) /ferr
+   return np.sum( abs(nres) )
+
+def modelCBV(p,bvectors):
+   return np.dot(p,bvectors)
+   
