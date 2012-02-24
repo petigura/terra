6e351c94e1120fc88e07c1d0fcc940e11d75b587
Erik Petigura <epetigura@berkeley.edu>
2012-02-23 21:22:49 -0800

    Added a --sim option.
    If it's set, the tLC files will be suffixed with the seed number, if not
    they will be suffixed with the KIC ID.

--- scripts/raw2lc.py
+++ scripts/raw2lc.py
@@ -8,13 +8,20 @@ parser = argparse.ArgumentParser(
     description='Read in RAWfile and preprocess')
 
 parser.add_argument('RAWfile',type=str)
+parser.add_argument('--sim',action='store_true',help='process with CBV')
+
 args   = parser.parse_args()
 tLCraw = atpy.TableSet(args.RAWfile,type='fits')
 tLC    = keplerio.prepLC(tLCraw,ver=False)
 
 dir = os.path.dirname(args.RAWfile)
+if args.sim:
+    tLCrawfile = 'tLC%04d.fits' % tLC.keywords['SEED']
+else:
+    tLCrawfile = 'tLC%09d.fits' % tLC.keywords['KEPLERID']
 
-tLCrawfile = 'tLC%04d.fits' % tLC.keywords['SEED']
+
+
 tLCrawfile = os.path.join(dir,tLCrawfile)
 tLC.write(tLCrawfile,overwrite=True,type='fits')
 print "raw2lc: Created %s" % (tLCrawfile)
