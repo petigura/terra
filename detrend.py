ed62de11c19d3676b6ddfde8820d2f70212d38f6
Erik Petigura <epetigura@berkeley.edu>
2012-05-14 16:35:08 -0700

    maskIntrp - default nContig=0 Added functions for segmentation spldtm - general function for spline detrending accepting a masked array

--- detrend.py
+++ detrend.py
@@ -6,7 +6,8 @@ from scipy.optimize import fmin
 import os
 
 import keptoy
-
+import atpy
+import copy
 
 dmi = 1.    # remove strips of data that are less than 1 day long.
 gmi = 1/24. # Let's not worry about gaps that are 1 hour long
@@ -37,30 +38,64 @@ def stitch(fl,tl,swd = 0.5):
 
     return fl,tl
 
-def nanIntrp(x0,y0,nContig=3):
+def maskIntrp(x0,y0,nContig=None):
     """
-    Use linear interpolation to fill in the nan-points.
+    Use linear interpolation to fill in masked pointss
 
-    nContig - Don't fill in nan values if there are more than a
-    certain number of contiguous ones.
+    Parameters
+    ----------
+    x0      : Independent var
+    y0      : Masked array.
+    nContig : Skip if masked region contains more than a certain
+              number of Contiguous masked points.  nContig = None means
+              interpolate through everything.
     """
 
     x,y = x0.copy(),y0.copy()
 
-    y = ma.masked_invalid(y)
-    x = ma.masked_array(x,mask=y.mask)
+    x = ma.masked_array(x,mask=y.mask,copy=True)
     xc,yc = x.compressed(),y.compressed()
 
     sp = UnivariateSpline(xc,yc,k=1,s=0)
 
     x.mask = ~x.mask
     sL = ma.notmasked_contiguous(x)
-    for s in sL:
-        nNans = s.stop-s.start
-        if nNans <= nContig:
-            y[s] = sp(x[s])
 
-    return x.data,y.data
+    if sL is None:
+        return x.data,y
+
+    if nContig is None:
+        for s in sL:
+            y[s] = sp(x[s])
+    else:
+        for s in sL:
+            nNans = s.stop-s.start
+            if nNans <= nContig:
+                y[s] = sp(x[s])
+
+    return x.data,y
+
+def dt(t0):
+
+    t = copy.deepcopy(t0)
+    fm = ma.masked_array(t.f,mask=t.fmask)
+    tm = ma.masked_array(t.TIME,mask=t.fmask)
+
+    label = sepseg(tm)
+
+    sL = ma.notmasked_contiguous(label)
+
+    # If there is only one slice.
+    if type(sL) == slice: 
+        sL = [sL]
+
+    id = sL2id(sL)
+
+    tnd = fm.copy()
+    temp = [spldtm(tm[s],fm[s]) for s in sL]
+    temp = ma.hstack(temp)
+    tnd[id] = temp
+    return fm-tnd
 
 def segfitm(t,fm,bv):
     """
@@ -134,20 +169,133 @@ def cbvseg(tm,segsep=1):
 
     return ma.notmasked_contiguous(tm)
 
-def spldt(t,f,lendt=10):
+def sepseg(tm0,tsep=1):
     """
-    Spline detrending
+    Seperate Segments
+
+    Assign a label to segments.  Segments can have gaps no longer than
+    tsep days.
+
+    Parameters
+    ----------
+
+    tm   : masked array with time.  Gaps are labeled with mask.
+    tsep : Segments can have gaps no longer than tsep.
+
+    Returns
+    -------
+    
+    label : Masked array.  Label identifies which segment a given point 
+            belongs to.  Masked elements do not belong to any segment  
+
     """
 
-    fm = ma.masked_invalid(f)
-    assert fm.count() == fm.size,"No nans allowed."
+    # Copy to prevent problems from changing the mask in place
+    tm = tm0.copy() 
+    label = np.zeros(tm.size)
+    label[:] = np.nan
+
+    nsep = tsep / keptoy.lc
+
+    gap = ma.masked_array(tm.data,mask=~tm.mask,copy=True)
+    nsep = tsep / keptoy.lc
+    gapSlice = ma.notmasked_contiguous(gap)
+
+    if gapSlice is None:
+        label[:] = 0 
+        return label
+        
+    for g in gapSlice:
+        if g.stop-g.start < nsep:
+            tm.mask[g] = False
+
+    sL = ma.notmasked_contiguous(tm)
+    nseg = len(sL)
+
+    for i in range(nseg):
+        s = sL[i]
+        label[s] = i
+
+    label = ma.masked_invalid(label)
+    return label
+    
+def joinseg(lab1,lab2):
+    """
+    Join segments
+
+    Create a new segment list from two.
+    
+    Parameters
+    ----------
+    
+    lab1 : Labels define as per sepseg
+    """
+    tup0 = (lab1[~labj.mask][0],lab2[~labj.mask][0])
+    j = 0
+    for i in range(labj.size):
+        if ~labj.mask[i]:
+            tup = (lab1[i],lab2[i])
+            if tup != tup0:
+                j = j + 1
+                tup0 = tup
+            labj[i] = j
+
+    return labj
+
+def sL2id(sL):
+    """
+    Convert a slice list to indecies.
+    """
+    return np.hstack([np.mgrid[s] for s in sL])
+
+def spldtm(t,fm,lendt=10):
+    """
+    Spline masked detrending.
+
+    Compute a smooth detrend ignoring the masked points.
+
+
+    Parameters
+    ----------
+    t     : ind var
+    fm    : dep var (can be masked array)
+    lendt : see spldt
+
+    Returns
+    -------
+    tnd   : Best fit trend with the same mask as fm.
+    
+    """
+    tm  = ma.masked_array(t,mask=fm.mask)
+    tnd = ma.masked_array(fm,copy=True)
+    x,y = tm.compressed(),fm.compressed()
+    tnd[~fm.mask] = spldt(x,y,lendt=lendt).astype('float32')
+    return tnd 
+
+def spldt(t,f,lendt=10):
+    """
+    Spline detrending.
+
+    The number of knots are chosen such that timescales longer than
+    lendth are removed.
+
+    Parameters
+    ----------
+    
+    t     : independent variable
+    f     : depedent variable
+    lendt : length of timescale to remove.
+    """
+
+    assert f[np.isnan(f)].size == 0, "nans screw up spline."
 
     tbase = t.ptp()
     nknots = np.floor(tbase / lendt) + 1
     tk = np.linspace(t[0],t[-1],nknots)
     tk = tk[1:-1]
     sp = LSQUnivariateSpline(t,f,tk)
-    return sp(t)
+    tnd = sp(t)
+    return tnd
 
 def bvfit(t,f,ferr,bv):
     """
