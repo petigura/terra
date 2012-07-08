8f799a7c416430e0ca6b02c931077d8600775f3d
Erik Petigura <epetigura@berkeley.edu>
2012-07-08 10:11:54 -0700

    Modified tfind behavior for short periods
    Added:
    - remShuffle
    - FFA
    - FFAShiftAdd
    - FFAGroupShiftAdd
    - FFAButterfly
    - XWrap2.  Extends array so it's a power of 2.
    
    Modified
    - ep and pep works with FFA now

--- tfind.py
+++ tfind.py
@@ -80,7 +80,7 @@ def isfilled(t,fm,twd):
 
     return filled
 
-def XWrap(x,ifold,fill_value=0):
+def XWrap(x,Pcad0,rem,fill_value=0,pow2=False):
     """
     Extend and wrap array.
     
@@ -91,7 +91,9 @@ def XWrap(x,ifold,fill_value=0):
     ----------
 
     x     : input
-    ifold : Wrap array after ifold indecies.
+    Pcad0 : Base period
+    rem   : The actual period can be longer by rem/nrow
+    pow2  : If true, pad out nRows so that it's the next power of 2.
 
     Return
     ------
@@ -101,8 +103,9 @@ def XWrap(x,ifold,fill_value=0):
     """
 
     ncad = x.size # Number of cadences
-    nrow = int(np.floor(ncad/ifold) + 1)
-    nExtend = nrow * ifold - ncad # Pad out remainder of array with 0s.
+    # for some reason np.ceil(ncad/Pcad0) doesn't work!
+    nrow = int( np.floor(ncad/Pcad0) +1 )
+    nExtend = nrow * Pcad0 - ncad # Pad out remainder of array with 0s.
 
     if type(x) is np.ma.core.MaskedArray:
         pad = ma.empty(nExtend)
@@ -112,9 +115,92 @@ def XWrap(x,ifold,fill_value=0):
         pad = np.empty(nExtend) 
         pad[:] = fill_value
         x = np.hstack( (x ,pad) )
+
     xwrap = x.reshape( nrow,-1 )
+    idShf = remShuffle(xwrap.shape,rem)
+    xwrap = xwrap[idShf]
 
     return xwrap
+
+
+def XWrap2(x,P0,fill_value=0,pow2=False):
+    """
+    Extend and wrap array.
+    
+    Fold array every y indecies.  There will typically be a hanging
+    part of the array.  This is padded out.
+
+    Parameters
+    ----------
+
+    x     : input
+    P0    : Base period, units of elements
+    pow2  : If true, pad out nRows so that it's the next power of 2.
+
+    Return
+    ------
+
+    xwrap : Wrapped array.
+
+    """
+
+    ncad = x.size # Number of cadences
+    # for some reason np.ceil(ncad/P0) doesn't work!
+    nrow = int( np.floor(ncad/P0) +1 )
+    nExtend = nrow * P0 - ncad # Pad out remainder of array with 0s.
+
+    if type(x) is np.ma.core.MaskedArray:
+        pad = ma.empty(nExtend)
+        pad.mask = True
+        x = ma.hstack( (x ,pad) )
+    else:    
+        pad = np.empty(nExtend) 
+        pad[:] = fill_value
+        x = np.hstack( (x ,pad) )
+
+    xwrap = x.reshape( nrow,-1 )
+
+    if pow2:
+        k = np.ceil(np.log2(nrow)).astype(int)
+        nrow2 = 2**k
+        fill    = ma.empty( (nrow2-nrow,P0) )
+        fill[:] = fill_value
+        fill.mask=True
+        xwrap = ma.vstack([xwrap,fill])
+
+    return xwrap
+
+
+def remShuffle(shape,rem):
+    """
+    Remainder shuffle
+
+    For a 2-D array with shape (Pcad0,nrow), this rearanges the
+    indecies such that the last row is shifted by rem.  rem can be any
+    integer between 0 and Pcad0-1
+    
+    Parameters
+    ----------
+    shape : Shape of array to be shuffled.
+    rem   : Shift the last row by rem.
+
+    Returns
+    -------
+    id    : Shuffled indecies.
+
+    """
+    nrow,ncol = shape
+
+#    assert type(rem) == int, 'rem must be an integer'
+    assert (rem >= 0) & (rem<=ncol), 'rem must be >= 0 and <= ncol '
+
+    irow,icol = np.mgrid[0:nrow,0:ncol]
+    colshift  = np.linspace(0,rem,nrow)
+    colshift  = np.round(colshift).astype(int)
+    for i in range(nrow):
+        icol[i] = np.roll(icol[i],-colshift[i])
+
+    return irow,icol
 
 
 def perGrid(tbase,ftdurmi,Pmin=100.,Pmax=None):
@@ -163,7 +249,7 @@ def perGrid(tbase,ftdurmi,Pmin=100.,Pmax=None):
     PG = np.array(PG)
     return PG
 
-def P2Pcad(PG0):
+def P2Pcad(PG0,ncad):
     """
     Convert units of period grid from days to cadences
 
@@ -172,10 +258,12 @@ def P2Pcad(PG0):
     """
     assert type(PG0) is np.ndarray, "Period Grid must be an array"
 
-    PcadG = (np.round(PG0/lc)).astype(int)
-    PG = PcadG * lc
+    PcadG0 = np.floor(PG0/keptoy.lc).astype(int)
+    nrow   = np.ceil(ncad/PcadG0).astype(int)+1
+    remG   = np.round((PG0/keptoy.lc-PcadG0)*nrow).astype(int)
 
-    return PcadG,PG
+    PG     = (PcadG0 + 1.*remG / nrow)*lc
+    return PcadG0,remG,PG
 
 def mtd(t,f,isStep,fmask,twd):
     """
@@ -241,18 +329,18 @@ def tdpep(t,fm,isStep,PG0):
     assert fm.fill_value ==0
     # Determine the grid of periods that corresponds to integer
     # multiples of cadence values
-    PcadG,PG = P2Pcad(PG0)
+    ncad = fm.size
+    PcadG,remG,PG = P2Pcad(PG0,ncad)
        
     # Initialize tdur grid.  
     twdMi = a2tdur( P2a( PG[0 ] ) ) /keptoy.lc
     twdMa = a2tdur( P2a( PG[-1] ) ) /keptoy.lc
     twdG = np.round(np.linspace(twdMi,twdMa,4)).astype(int)
-
     rec2d = []
     noise = []
     for twd in twdG:
         dM = mtd(t,fm.filled(),isStep,fm.mask,twd)
-        rec2d.append( pep(t[0],dM,PcadG) )
+        rec2d.append( pep(t[0],dM,PcadG,remG) )
 
         # Noise per transit 
         mad = ma.abs(dM)
@@ -276,8 +364,6 @@ def pep(t0,dM,PcadG):
     """
     Period-Epoch
 
-    Wraps ep over a grid of periods.  It marginalizes over epoch.
-
     Parameters
     ----------
     t0    : time of first dM[0].
@@ -286,17 +372,12 @@ def pep(t0,dM,PcadG):
     """
 
     func = lambda Pcad: ep(t0,dM,Pcad)
-    resL = map(func,PcadG)
-
-    # Marginalize over epoch.
-    func = lambda r,i : (r['epoch'][i],r['fom'][i],r['count'][i])
-    iMa = [ np.argmax(r['fom']) for r in resL ]
-    res = map(func,resL,iMa)
-    res = array(res,dtype=[('epoch',float),('fom',float),('count',int)])
+    res = map(func,PcadG)
+    res = np.hstack(res)
 
     return res
 
-def ep(t0,dM,Pcad):
+def ep(t0,dM,Pcad0):
     """
     Search in Epoch.
 
@@ -313,32 +394,33 @@ def ep(t0,dM,Pcad):
     - 'win'    : Which epochs passed (window function)
     """
     
-    dMW = XWrap(dM,Pcad)
-    nt,ne = dMW.shape
-    epoch = np.arange(ne,dtype=float)/ne * Pcad *lc + t0
+    dMW = XWrap2(dM,Pcad0,pow2=True)
+    M   = dMW.shape[0]  # number of rows
 
-    # Checks that all of the SES pass a minimum threshold, miSES.
-    vcount = (~dMW.mask).astype(int).sum(axis=0)
-    sig = (dM > miSES).astype(int)
-    sigW = XWrap(sig,Pcad,fill_value=0)
-    nsig = sigW.sum(axis=0)
-    bsig = (nsig == vcount).astype(float)
+    idCol = np.arange(Pcad0,dtype=int)   # id of each column
+    idRow = np.arange(M,dtype=int)   # id of each row
 
-    # Removes transits in gaps.
-    win = (vcount >= miTransMES)  
+    epoch = idCol.astype(float) * lc + t0
+    Pcad  = Pcad0 + idRow.astype(float) / (M - 1)
 
-    # Zero out MES values that fail the cuts.
-    cut = win & (nsig == vcount) 
-    win = win.astype(float)
-    cut = cut.astype(float)
-    fom = cut*dMW.mean(axis=0)
+    dMW.fill_value=0
+    data = dMW.filled()
+    mask = (~dMW.mask).astype(int)
 
-    res = {
-        'fom'    : fom     ,
-        'epoch'  : epoch   ,
-        'win'    : win     ,
-        'count'  : vcount  ,
-        }
+    sumF   = FFA(data) # Sum of array elements folded on P0, P0 + i/(1-M)
+    countF = FFA(mask) # Number of valid data points
+    meanF  = sumF/countF
+
+    names = ['mean','count','epoch','Pcad']
+    dtype = zip(names,[float]*4)
+    res   = np.empty(M,dtype=dtype)
+
+    # Take maximum epoch
+    idColMa      = meanF.argmax(axis=1)
+    res['mean']  = meanF[idRow,idColMa]
+    res['count'] = countF[idRow,idColMa]
+    res['epoch'] = epoch[idColMa]
+    res['Pcad']  = Pcad
 
     return res
 
@@ -363,5 +445,157 @@ def tdmarg(rec2d):
 
     return rec
 
+def FFA(XW):
+    """
+    Fast Folding Algorithm
 
+    Consider an evenly-spaced timeseries of length N.  We can fold it
+    on P0, by creating a new array XW, shape = (P0,M), M = N/P0.
+    There are M ways to fold XW, yielding the following periods
+
+    P = P0 + i / M - 1
+
+    Where i ranges from 0 to M-1.  Summing all these values requires P
+    * M**2 = N**2 / P0 sums.  If M is a power of 2, the FFA eliminates
+    some of the redundant summing and requires only N log2 (N/P0)
+    sums.
+
+    Algorithm
+    ---------
+    The columns of XW are shifted and summed in a pairwise fashion.
+
+    - `FFAButterfly` : for a group of size nGroup, `FFAButterfly`
+      computes the amount pairwise combinations of rows and the amount
+      the second row is shifted.
+    - `FFAShiftAdd` : Adds the rows in the manner specified by
+      `FFAButterfly`
+        
+    Parameters
+    ----------
+    XW : Wrapped array folded on P0.  shape(XW) = (P0,M) and M must be
+         a power of 2
+    
+    Returns
+    -------
+    XWFS : XW Folded and Summed. 
+
+    References
+    ----------
+    [1] Staelin (1969)
+    [2] Kondratiev (2009)
+    """
+
+    # Make sure array is the right shape.
+    nRow,P0  = XW.shape
+    nStage   = np.log2(nRow)
+    assert np.allclose(nStage,np.round(nStage)),"nRow must be power of 2"    
+    nStage = int(nStage)
+
+    XWFS = XW.copy()
+    for stage in range(1,nStage+1):
+        XWFS = FFAShiftAdd(XWFS,stage) 
+    return XWFS
+
+
+
+def FFAButterfly(stage):
+    """
+    FFA Butterfly
+
+    The FFA adds pairs of rows A and B. B is shifted.  FFAButterfly
+    computes A, B, and the amount by which B is shifted
+
+    Parameters
+    ----------
+    
+    stage : FFA builds up by stages.  Stage 1 shuffles adjacent rows
+            (nRowGroup = 2) while stage K shuffles all M = 2**K rows
+            (nRowGroup = M).
+
+    """
+    nRowGroup = 2**stage
+
+    Arow  = np.empty(nRowGroup,dtype=int)
+    Brow  = np.empty(nRowGroup,dtype=int)
+    Bshft = np.empty(nRowGroup+2,dtype=int)
+
+    Arow[0::2] = Arow[1::2] = np.arange(0,nRowGroup/2)
+    Brow[0::2] = Brow[1::2] = np.arange(nRowGroup/2,nRowGroup)
+    Bshft[0::2] = Bshft[1::2] = np.arange(0,nRowGroup/2+1)
+    Bshft =  Bshft[1:-1]
+
+    return Arow,Brow,Bshft
+
+def FFAGroupShiftAdd(group0,Arow,Brow,Bshft):
+    """
+    FFA Shift and Add
+
+    Add the rows of `group` to each other.
+    
+    Parameters
+    ----------
+
+    group0 : Initial group before shuffling and adding. 
+             shape(group0) = (M,P0) where M is a power of 2.
+
+    """
+    nRowGroup = group0.shape[0]
+    group     = np.empty(group0.shape)
+
+    sizes = np.array([Arow.size, Brow.size, Bshft.size])
+    assert (sizes == nRowGroup).all() , 'Number of rows in group must agree with butterfly output'
+
+    for iRow in range(nRowGroup):
+        iA = Arow[iRow]
+        iB = Brow[iRow]
+        Bs = Bshft[iRow]
+
+        A = group0[iA] 
+        B = group0[iB]
+        if Bs!=0:
+            np.roll(B,-Bshft[iRow])
+
+        group[iRow] = A + B
+
+    return group 
+
+def FFAShiftAdd(XW0,stage):
+    """
+    FFA Shift and Add
+
+    Shuffle pairwise add the rows of the FFA data array corresponding
+    to stage
+    
+    Parameters
+    ----------
+    XW0   : array
+    stage : The stage in the FFA.  An integer ranging from 1 to K
+            where 2**K = M
+            
+    Returns
+    -------
+    XW    : Shifted and added array
+
+
+    Test Cases
+    ----------
+
+    >>> tfind.FFAShiftAdd(eye(4),1)
+    >>> array([[ 1.,  1.,  0.,  0.],
+               [ 2.,  0.,  0.,  0.],
+               [ 0.,  0.,  1.,  1.],
+               [ 0.,  0.,  2.,  0.]])
+    """
+    nRow      = XW0.shape[0]
+    nRowGroup = 2**stage
+    nGroup    = nRow/nRowGroup
+    XW        = np.empty(XW0.shape)
+    Arow,Brow,Bshft = FFAButterfly(stage)
+    for iGroup in range(nGroup):
+        start = iGroup*nRowGroup
+        stop  = (iGroup+1)*nRowGroup
+        sG = slice(start,stop)
+        XW[sG] = FFAGroupShiftAdd(XW0[sG],Arow,Brow,Bshft)
+
+    return XW
 
