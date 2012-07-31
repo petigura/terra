72427761843a53abeab4204b4639101f73e03170
Erik Petigura <petigura@ln000.scs.berkeley.edu>
2012-07-30 22:10:01 -0700

    mc

--- scripts/splith5.py
+++ scripts/splith5.py
@@ -6,18 +6,17 @@ Split h5 file into individual files
 from argparse import ArgumentParser
 import h5plus
 import h5py
-from numpy import ma
-import numpy as np
-import cotrend
-import keplerio
+import os
+
 from matplotlib import mlab
 
 prsr = ArgumentParser()
 prsr.add_argument('inp',type=str, help='input files')
 args  = prsr.parse_args()
 
-file = args.inp
-h0    = h5py.File(file)
+finp  = args.inp
+h0    = h5py.File(finp)
+dir   = os.path.dirname(finp)
 
 # Figure out which datasets we vtacking
 kTwoDim = ''
@@ -28,13 +27,15 @@ for k in h0.iterkeys():
 TwoDimdtype = h0[kTwoDim].dtype
 kicdtype    = h0['KIC'].dtype
 nrows       = h0['KIC'].shape[0]
+
 for i in range(nrows):
-    ext = file[file.find('.'):]
+    ext = finp[finp.find('.'):]
     kic = h0['KIC'][i]
     fout = "%09d%s" % (kic,ext)
+    pout = os.path.join(dir,fout)
     print fout
 
-    h = h5plus.File(fout)
+    h = h5plus.File(pout)
     h.create_dataset(kTwoDim,data=h0[kTwoDim][i])
     h.close()
 
