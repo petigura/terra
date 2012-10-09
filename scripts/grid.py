6610e959d102993061170dd7221b5e37854d7608
Erik Petigura <epetigura@berkeley.edu>
2012-10-08 17:47:52 -0700

    Deleted deprecated scripts

--- scripts/grid.py
+++ scripts/grid.py
@@ -1,24 +1,29 @@
-import sim
-import argparse
-import atpy
+from argparse import ArgumentParser
 from numpy import ma
+import keptoy
+import h5py
+import h5plus
+import numpy as np
+import tfind
+import cotrend
+import config
 
-parser = argparse.ArgumentParser(description='Run grid search')
-parser.add_argument('inp',  type=str   , help='input file')
-parser.add_argument('out',  type=str   , help='output file')
-parser.add_argument('-psmp',type=float , default=0.25,dest='Psmp')
+P1 = int(np.floor(config.P1/keptoy.lc))
+P2 = int(np.floor(config.P2/keptoy.lc))
 
-args = parser.parse_args()
+prsr = ArgumentParser(description='Run grid search')
+prsr.add_argument('inp',type=str,help='input file')
+prsr.add_argument('out',type=str,help='output file')
+args = prsr.parse_args()
 
-tLC = atpy.Table(args.inp,type='fits')
-t = tLC.t
-f = tLC.fdt - tLC.fcbv
+h5    = h5py.File(args.inp,'r+')
+h5out = h5plus.File(args.out)
 
-fm = ma.masked_array(f,mask=tLC.fmask,fill_value=0)    
-tRES = sim.grid(t,fm,tLC.isStep,Psmp =args.Psmp)
-tRES.comments = "Table with the simulation results"
-tRES.table_name = "RES"
-tRES.keywords = tLC.keywords
+lc  = h5['mqcal'][:]
 
-tRES.write(args.out,type='fits',overwrite=True)
-print "grid: Created %s" % args.out
+fm  = ma.masked_array(lc['fcal'],lc['fmask'],fill_value=0)
+rtd = tfind.tdpep(lc['t'],fm,P1,P2,config.twdG)
+r   = tfind.tdmarg(rtd)
+h5out['RES'] = r
+print "grid: Created %s" % h5out
+h5out.close()
