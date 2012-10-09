7cff24b62492118002226af9d08fc39794e61fbb
Erik Petigura <epetigura@berkeley.edu>
2012-10-08 18:22:53 -0700

    inject takes Lightcurve class

--- scripts/inject.py
+++ scripts/inject.py
@@ -5,6 +5,7 @@ import pandas
 import glob
 import keptoy
 from numpy import ma
+import os
 
 parser = argparse.ArgumentParser(
     description='Inject transit into template light curve.')
@@ -13,29 +14,24 @@ parser.add_argument('inp',  type=str   , help='input folder')
 parser.add_argument('out',  type=str   , help='output folder')
 parser.add_argument('parfile', type=str, help='file with the transit parameters')
 parser.add_argument('parrow',  type=int , help='row of the transit parameter')
+
 args = parser.parse_args()
-inp =args.inp
+inp = args.inp
 out = args.out
-stars = pandas.read_table(args.parfile,sep='\s*',index_col=0)
+store = HDFStore('simPar.h5')
+simPar = store['simPar']
 
 d = dict(stars.ix[args.parrow])
-d['skic'] = "%09d" % d['skic']
+inpfile = os.path.join(inp,d['skic']+'.h5')
+outfile = os.path.join(out,d['bname']+'.h5')
 
-
-searchstr = inp+'Q*/kplr%s-*_llc.fits' % (d['skic'] ) 
-
-files = glob.glob(searchstr)
-for file in files:
-    t = atpy.Table(file,type='fits')
-    fmed = ma.masked_invalid(t.data['SAP_FLUX'])
-    fmed = ma.median(fmed)
-
+os.system('cp %s %s' % (inpfile,outfile ) )
+raw = prepro.Lightcurve(outfile)['raw']
+for i in raw.items():
+    ds = i[0]
+    quarter = q[1]
+    
     ft = keptoy.synMA(d,t.TIME)
-    ft *= fmed # Scale the flux to the median quarterly flux
-    t.data['SAP_FLUX'] += ft
+    ds['f'] += ft
 
-    outfile =file.replace(inp,out)    
-    outfile = outfile[:outfile.find('kplr')]
-    outfile += '%s_%06d.fits' % (d['skic'],args.parrow) 
-    t.write(outfile,type='fits',overwrite=True)
-    print "inject: Created %s"  % outfile
+print "inject: Created %s"  % outfile
