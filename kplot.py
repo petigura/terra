3c4cbc72b259126b0bc04cabe0155533f732b614
Erik Petigura <epetigura@berkeley.edu>
2012-11-02 19:58:55 -0700

    kplot is no longer Object Oriented

--- kplot.py
+++ kplot.py
@@ -22,174 +22,190 @@ from numpy import ma
 
 tprop = dict(size=10,name='monospace')
 
-class Peak(tval.Peak):
-    def plot_diag(self):
-        """
-        Print a 1-page diagnostic plot of a given pk.
-        """
+#def __init__(pk,*args,**kwargs):
+#    tval.Peak.__init__(pk,*args,**kwargs)
+#    try: 
+#        pk.keys().index('RES')
+#    except ValueError:
+#        pk['RES'] = pk['res']
+#
+#    try: 
+#        pk.keys().index('mqcal')
+#    except ValueError:
+#        pk['mqcal'] = pk['lc']
 
-        fig = plt.figure(figsize=(20,12))
-        gs = GridSpec(8,10)
-        axGrid     = fig.add_subplot(gs[0,0:8])
-        axStack    = fig.add_subplot(gs[3: ,0:8])
-        axPFAll    = fig.add_subplot(gs[1,0:8])
-        axPF       = fig.add_subplot(gs[2,0:4])
-        axPF180    = fig.add_subplot(gs[2,4:8],sharex=axPF,sharey=axPF)
-        axScar     = fig.add_subplot(gs[0,-1])
-        axSES      = fig.add_subplot(gs[1,-1])
-        axSeason   = fig.add_subplot(gs[2,-1])
-        axAutoCorr = fig.add_subplot(gs[3,-1])
 
-        plt.sca(axGrid)
-        self.plotGrid()
-        at = AnchoredText('Periodogram',prop=tprop, frameon=True,loc=2)
-        axGrid.add_artist(at)
-        axGrid.xaxis.set_ticks_position('top')
-        plt.title('Period (days)')
-        plt.ylabel('MES')
+def plot_diag(pk):
+    """
+    Print a 1-page diagnostic plot of a given pk.
+    """
 
-        plt.sca(axPFAll)
-        plt.plot(self['tPF'],self['fmed'],',',alpha=.5)
-        plt.plot(self['bx1'],self['by1'],'o',mew=0)
-        plt.plot(self['bx5'],self['by5'],'.',mew=0)
-        y = self['fmed']
-        axPFAll.set_ylim( (np.percentile(y,5),np.percentile(y,95) ) )
+    fig = plt.figure(figsize=(20,12))
+    gs = GridSpec(8,10)
+    axGrid     = fig.add_subplot(gs[0,0:8])
+    axStack    = fig.add_subplot(gs[3: ,0:8])
+    axPFAll    = fig.add_subplot(gs[1,0:8])
+    axPF       = fig.add_subplot(gs[2,0:4])
+    axPF180    = fig.add_subplot(gs[2,4:8],sharex=axPF,sharey=axPF)
+    axScar     = fig.add_subplot(gs[0,-1])
+    axSES      = fig.add_subplot(gs[1,-1])
+    axSeason   = fig.add_subplot(gs[2,-1])
+    axAutoCorr = fig.add_subplot(gs[3,-1])
 
-        plt.sca(axPF180)
-        self.plotPF(180)
-        cax = plt.gca()
-        cax.xaxis.set_visible(False)
-        cax.yaxis.set_visible(False)
-        at = AnchoredText('Phase Folded LC + 180',prop=tprop,frameon=True,loc=2)
-        cax.add_artist(at)
 
-        plt.sca(axPF)
-        self.plotPF(0)
-        cax = plt.gca()
-        cax.xaxis.set_visible(False)
-        cax.yaxis.set_visible(False)
-        at = AnchoredText('Phase Folded LC',prop=tprop,frameon=True,loc=2)
-        cax.add_artist(at)
-        df = self.attrs['pL0'][0]**2
-        plt.ylim(-5*df,3*df)
 
-        plt.sca(axStack)
-        self.plotSES()
-        plt.xlabel('Phase')
-        plt.ylabel('SES (ppm)')
+    print pk.items()
 
-        plt.sca(axScar)
-        sketch.scar(self.res)
-        plt.gca().xaxis.set_visible(False)
-        plt.gca().yaxis.set_visible(False)
+    plt.sca(axGrid)
+    plotGrid(pk)
+    at = AnchoredText('Periodogram',prop=tprop, frameon=True,loc=2)
+    axGrid.add_artist(at)
+    axGrid.xaxis.set_ticks_position('top')
+    plt.title('Period (days)')
+    plt.ylabel('MES')
 
-        axSeason.set_xlim(-1,4)
-        rses = self['SES']
-        axSES.plot(rses['tnum'],rses['ses']*1e6,'.')
-        axSES.xaxis.set_visible(False)
-        at = AnchoredText('Transit SES',prop=tprop, frameon=True,loc=2)
-        axSES.add_artist(at)
+    plt.sca(axPFAll)
+    plt.plot(pk['tPF'],pk['fmed'],',',alpha=.5)
+    plt.plot(pk['bx1'],pk['by1'],'o',mew=0)
+    plt.plot(pk['bx5'],pk['by5'],'.',mew=0)
+    y = pk['fmed']
+    axPFAll.set_ylim( (np.percentile(y,5),np.percentile(y,95) ) )
 
-        axSeason.plot(rses['season'],rses['ses']*1e6,'.')
-        axSeason.xaxis.set_visible(False)
-        at = AnchoredText('Season SES',prop=tprop, frameon=True,loc=2)
-        axSeason.add_artist(at)
+    plt.sca(axPF180)
+    plotPF(pk,180)
+    cax = plt.gca()
+    cax.xaxis.set_visible(False)
+    cax.yaxis.set_visible(False)
+    at = AnchoredText('Phase Folded LC + 180',prop=tprop,frameon=True,loc=2)
+    cax.add_artist(at)
 
-        plt.sca(axAutoCorr)
-        self.plotAutoCorr()
-        plt.gca().xaxis.set_visible(False)
-        plt.gca().yaxis.set_visible(False)
-        
-        plt.gcf().text( 0.75, 0.05, self.diag_leg() , size=10, name='monospace',
-                        bbox=dict(visible=True,fc='white'))
-        plt.tight_layout()
-        plt.gcf().subplots_adjust(hspace=0.01,wspace=0.01)
+    plt.sca(axPF)
+    plotPF(pk,0)
+    cax = plt.gca()
+    cax.xaxis.set_visible(False)
+    cax.yaxis.set_visible(False)
+    at = AnchoredText('Phase Folded LC',prop=tprop,frameon=True,loc=2)
+    cax.add_artist(at)
+    df = pk.attrs['pL0'][0]**2
+    plt.ylim(-5*df,3*df)
 
-    ###########################
-    # Helper fuctions to plot #
-    ###########################
-    def plotAutoCorr(self):
-        plt.xlabel('Displacement')
-        plt.plot(self['lag'],self['corr'])
+    plt.sca(axStack)
+    plotSES(pk)
+    plt.xlabel('Phase')
+    plt.ylabel('SES (ppm)')
 
-    def plotPF(self,ph):
-        PF      = self['lcPF%i' % ph]
-        x = PF['tPF']
+    plt.sca(axScar)
+    sketch.scar(pk['RES'])
+    plt.gca().xaxis.set_visible(False)
+    plt.gca().yaxis.set_visible(False)
 
-        try:
-            plt.plot(x,PF['fPF'],',',color='k')
-        except:
-            pass
+    axSeason.set_xlim(-1,4)
+    rses = pk['SES']
+    axSES.plot(rses['tnum'],rses['ses']*1e6,'.')
+    axSES.xaxis.set_visible(False)
+    at = AnchoredText('Transit SES',prop=tprop, frameon=True,loc=2)
+    axSES.add_artist(at)
 
-        try:
-            plt.plot(x,PF['fit'],lw=3,color='c')
-        except:
-            pass
+    axSeason.plot(rses['season'],rses['ses']*1e6,'.')
+    axSeason.xaxis.set_visible(False)
+    at = AnchoredText('Season SES',prop=tprop, frameon=True,loc=2)
+    axSeason.add_artist(at)
 
-        try:
-            x,y = self['bx%i_%i'% (ph,5)],self['by%i_%i'% (ph,5)]
-            plt.plot(x,y,'o',mew=0,color='red')
-        except:
-            pass
+    plt.sca(axAutoCorr)
+    #!pk.plotAutoCorr()
+    plt.gca().xaxis.set_visible(False)
+    plt.gca().yaxis.set_visible(False)
 
-    def plotSES(self):
-        df = self.attrs['pL0'][0]**2
-        sketch.stack(self.t,self.dM*1e6,self.P,self.t0,step=3*df*1e6)
-        plt.autoscale(tight=True)
+    plt.gcf().text( 0.75, 0.05, pk.diag_leg() , size=10, name='monospace',
+                    bbox=dict(visible=True,fc='white'))
+    #plt.tight_layout()
+    plt.gcf().subplots_adjust(hspace=0.01,wspace=0.01)
 
-    def plotGrid(self):
-        x = self.res['Pcad']*config.lc
-        plt.plot(x,self.res['s2n'])
-        id = np.argsort( np.abs(x - self.P) )[0]
-        plt.plot(x[id],self.res['s2n'][id],'ro')
-        plt.autoscale(tight=True)
+###########################
+# Helper fuctions to plot #
+###########################
+def plotAutoCorr(pk):
+    plt.xlabel('Displacement')
+    plt.plot(pk['lag'],pk['corr'])
 
-    def plotMed(self):
-        lc = self['mqcal'][:]
-        t = lc['t']
-        fmed = ma.masked_array(self['fmed'][:],lc['fmask'])
-        P = self.attrs['P']
-        t0 = self.attrs['t0']
-        df = self.attrs['df']
-        sketch.stack(t,fmed*1e6,P,t0,step=5*df)
-        plt.autoscale(tight=True)
+def plotPF(pk,ph):
+    PF      = pk['lcPF%i' % ph]
+    x = PF['tPF']
 
-    def morton(self):
-        """
-        Print a 1-page diagnostic plot of a given pk.
-        """
+    try:
+        plt.plot(x,PF['fPF'],',',color='k')
+    except:
+        pass
 
-        fig = plt.figure(figsize=(20,12))
-        gs = GridSpec(4,4)
+    try:
+        plt.plot(x,PF['fit'],lw=3,color='c')
+    except:
+        pass
 
-        axPF       = fig.add_subplot(gs[0,0:2])
-        axPF180    = fig.add_subplot(gs[0,2:],sharex=axPF,sharey=axPF)
-        axStack    = fig.add_subplot(gs[1:,:])
+    try:
+        x,y = pk['bx%i_%i'% (ph,5)],pk['by%i_%i'% (ph,5)]
+        plt.plot(x,y,'o',mew=0,color='red')
+    except:
+        pass
 
-        plt.sca(axPF180)
-        self.plotPF(180)
-        cax = plt.gca()
-        cax.xaxis.set_visible(False)
-        cax.yaxis.set_visible(False)
-        at = AnchoredText('Phase Folded LC + 180',prop=tprop,frameon=True,loc=2)
-        cax.add_artist(at)
+def plotSES(pk):
+    df = pk.attrs['pL0'][0]**2
+    sketch.stack(pk.t,pk.dM*1e6,pk.attrs['P'],pk.attrs['t0'],step=3*df*1e6)
+    plt.autoscale(tight=True)
 
-        plt.sca(axPF)
-        self.plotPF(0)
-        cax = plt.gca()
-        at = AnchoredText('Phase Folded LC',prop=tprop,frameon=True,loc=2)
-        cax.add_artist(at)
-        plt.xlabel('t - t0 (days)')
-        plt.ylabel('flux')
-        df = self.attrs['df']*1e-6
-        plt.ylim(-5*df,3*df)
+def plotGrid(pk):
+    x = pk['RES']['Pcad']*config.lc
+    plt.plot(x,pk['RES']['s2n'])
+    id = np.argsort( np.abs(x - pk.attrs['P']) )[0]
+    plt.plot(x[id],pk['RES']['s2n'][id],'ro')
+    plt.autoscale(tight=True)
 
-        plt.sca(axStack)
-        self.plotMed()
-        plt.xlabel('phase')
-        plt.ylabel('flux (ppm)')
+def plotMed(pk):
+    lc = pk['mqcal'][:]
+    t = lc['t']
+    fmed = ma.masked_array(pk['fmed'][:],lc['fmask'])
+    P = pk.attrs['P']
+    t0 = pk.attrs['t0']
+    df = pk.attrs['df']
+    sketch.stack(t,fmed*1e6,P,t0,step=5*df)
+    plt.autoscale(tight=True)
 
-#        plt.gcf().text( 0.85, 0.05, self.diag_leg() , size=10, name='monospace',
-#                        bbox=dict(visible=True,fc='white'))
-#        plt.tight_layout()
-        plt.gcf().subplots_adjust(hspace=0.21,wspace=0.05,left=0.05,right=0.99,bottom=0.05,top=0.99)
+def morton(pk):
+    """
+    Print a 1-page diagnostic plot of a given pk.
+    """
+
+    fig = plt.figure(figsize=(20,12))
+    gs = GridSpec(4,4)
+
+    axPF       = fig.add_subplot(gs[0,0:2])
+    axPF180    = fig.add_subplot(gs[0,2:],sharex=axPF,sharey=axPF)
+    axStack    = fig.add_subplot(gs[1:,:])
+
+    plt.sca(axPF180)
+    plotPF(pk,180)
+    cax = plt.gca()
+    cax.xaxis.set_visible(False)
+    cax.yaxis.set_visible(False)
+    at = AnchoredText('Phase Folded LC + 180',prop=tprop,frameon=True,loc=2)
+    cax.add_artist(at)
+
+    plt.sca(axPF)
+    plotPF(pk,0)
+    cax = plt.gca()
+    at = AnchoredText('Phase Folded LC',prop=tprop,frameon=True,loc=2)
+    cax.add_artist(at)
+    plt.xlabel('t - t0 (days)')
+    plt.ylabel('flux')
+    df = pk.attrs['df']*1e-6
+    plt.ylim(-5*df,3*df)
+
+    plt.sca(axStack)
+    plotMed(pk)
+    plt.xlabel('phase')
+    plt.ylabel('flux (ppm)')
+
+    #plt.gcf().text( 0.85, 0.05, pk.diag_leg() , size=10, name='monospace',
+    #                bbox=dict(visible=True,fc='white'))
+    #plt.tight_layout()
+    plt.gcf().subplots_adjust(hspace=0.21,wspace=0.05,left=0.05,right=0.99,bottom=0.05,top=0.99)
