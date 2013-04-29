d7df38d8499f9cacb6488f5ae7ab19a552043afc
Erik Petigura <epetigura@berkeley.edu>
2013-04-29 15:05:48 -0700

    mc

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,29 +1,26 @@
 import pandas,h5py
 import os,sys
-
 pp = pandas.read_csv('pp.csv',index_col=0)
 dL = []
-for i in pp.outfile:
+for i in pp.index:
+    outf = pp.ix[i]['outfile']
+    pd = dict(pp.ix[i])
     d = {}
-    d['outfile'] = i
+    d['outfile'] = outf
     try:
-        with h5py.File(i,mode='r') as h5:
-            l = h5.keys()
-            o = ''
-            if l.index('cal')!=0:
-                o = 'cal' 
-            if l.index('it0')!=0:
-                o = 'it0'
-            d['last'] = o
+        with h5py.File(outf,mode='r') as h5:
+            d['num_trans']  = h5.attrs['num_trans']
             ktop = 'P,autor,s2ncut,s2n,medSNR,t0,skic'.split(',')
             for k in ktop:
                 d[k]  = h5.attrs[k]
-            d['p0']   = h5['fit'].attrs['pL0'][0]
-            d['tau0'] = h5['fit'].attrs['pL0'][1]
-            d['b0']   = h5['fit'].attrs['pL0'][2]
+            d['p0']        = h5['fit'].attrs['pL0'][0]
+            d['tau0']      = h5['fit'].attrs['pL0'][1]
+            d['b0']        = h5['fit'].attrs['pL0'][2]
     except:
-        print i,sys.exc_info()[1]
+        print outf,sys.exc_info()[1]
+    if i%10==0:
+        print i
     dL.append(d)
-    
 df = pandas.DataFrame(dL)
 df.to_csv(os.getcwd()+'.out.csv')
+
