2ec32013551a705d2dcdef52cba7438be833451e
Erik Petigura <epetigura@berkeley.edu>
2014-09-22 19:10:43 -0700

    Major changes to mkModes.py
    - Uses the Ensemble Cotrending object
    - Check points with big h5 database
    - Saves Ensemble Calibrating object

--- code/py/scripts/mkModes.py
+++ code/py/scripts/mkModes.py
@@ -1,126 +1,86 @@
 """
 Wrapper around the spline detrending.
 """
+from argparse import ArgumentParser
+from glob import glob
+import os
+
+from astropy.io import fits
+import h5py
+import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
-
-from argparse import ArgumentParser
-import pandas as pd
-import numpy as np
-from numpy import ma
 from matplotlib.pylab import *
 
-import h5plus
-import h5py
-
-import cotrend
-import sys
-from config import path_phot,path_train,k2_projdir
-import stellar
 import photometry
+import h5plus
 import prepro
+from cotrend import EnsembleCalibrator,makeplots
+import k2_catalogs
+import cPickle as pickle
 
-algo = 'ICA'
-n_components = 8
-plot_basename = '%s/cotrend_robust%s_ncomp=%i' % (k2_projdir,algo,n_components) 
 
 parser = ArgumentParser(description='Ensemble calibration')
-args  = parser.parse_args()
-print "Reading training photometry from %s" % path_phot
-print "Saving modes to %s" % path_train
+parser.add_argument('fitsdir',type=str,help='directory with fits files')
+parser.add_argument('-f',action='store_true',
+                     help='Force re-creation of h5 checkpoint file')
+args = parser.parse_args()
 
-# Grab stars used for computing modes
-df0 = photometry.phot_vs_kepmag()
-dftr = df0.copy()
-dftr = dftr[dftr.logfmed_resid.between(-0.5,0.5)]
-#dftr = dftr[dftr.prog.str.contains('GKM|cool')]
-dftr = dftr.query('10 < kepmag < 13')
+# Loading up all the fits files takes some time. The first order of
+# business is to create a checkpoint.
 
-# Load up light curves
-lc = photometry.read_phot(path_phot, list(dftr.epic) )
+dir = args.fitsdir
+dirname = os.path.dirname(dir)
+h5file = "%s.h5" % (dirname)
 
-# Detrend light curves
-nlc = lc.shape[0]
-print "Detrending %i light curves" % nlc
+if not os.path.exists(h5file) or args.f:
+    fL = glob('%s/*.fits' % dirname)
+    nfL = len(fL)
+    print "loading up %i files" % nfL
 
-lc2 = []
-for i in range(nlc):
-    lc2+=[prepro.rdt(lc[i])]
-    if (i%100)==0:
-        print i
-lc = np.vstack(lc2) 
+    lcL = []
+    epicL = []
+
+    for i in range(nfL):
+        f = fL[i]
+        lc = photometry.read_crossfield_fits(fL[i])
+        lc = prepro.rdt(lc)
+        epic = os.path.basename(f).replace('.fits','')
+        if i%100==0:
+            print i
+
+        lcL+=[lc]
+        epicL+=[epic]
+
+    lc = np.vstack(lcL)
+    epic = np.array(epicL)
+    
+    print "Creating h5 checkpoint: %s" % h5file
+    with h5plus.File(h5file) as h5:
+        h5['lc'] = lc
+        h5['epic'] = epic
+
+# Load up files from h5 database
+print "Reading files from %s" % h5file
+with h5py.File(h5file) as h5:
+    lc = h5['lc'][:]
+    epic = h5['epic'][:]
 
 fdt = ma.masked_array(lc['fdt'],lc['fmask'])
+epic = epic.astype(int)
+dftr = pd.DataFrame(index=epic)
+dftr['epic'] = dftr.index
+targets = k2_catalogs.read_cat(return_targets=True)
+dftr = pd.merge(dftr,targets.drop_duplicates())
 
-# Mask out bad columns and rows
-intmask = (~fdt.mask).astype(int) # Mask as integers
-bcol = intmask.sum(axis=0)!=0 # True - good column
-brow = intmask.sum(axis=1)!=0 # True - good row
-fdt = fdt[:,bcol]
+import pdb;pdb.set_trace()    
+ec = EnsembleCalibrator(fdt,dftr)
+ec.robust_components(algo='PCA')
 
-for n,b in zip(['cadences','light curves'],[bcol,brow]):
-    print "%i %s masked out" % (b.size - b.sum(),n)
-print ""
-    
-fdt_sig = 1.48 * median(abs(fdt),axis=1)
-fdt_stand = fdt / fdt_sig[:,newaxis]
-M = fdt_stand.data.T.copy()
+ec.plot_basename = ec.plot_basename.replace('cotrend',dirname)
+makeplots(ec,savefig=True)
 
-U,V,icolin = cotrend.robust_components(M,algo=algo,n_components=n_components)
-cotrend.plot_PCs(U,V)
-
-U_clip = U.copy()
-U = np.zeros((bcol.size,U.shape[1]))
-U[bcol,:] = U_clip
-
-for kepmag in [10,11,12,14]:
-    clf()
-    cotrend.plot_modes_diag(U,V,kepmag)
-    gcf().savefig( plot_basename + '_kepmag=%i.png' % kepmag)
-
-M_clip = M[:,icolin]
-fdt_clip = fdt[icolin] # Clipout light curves that didn't make the
-nclip = len(icolin)
-A = [cotrend.bvfitm(fdt_clip[i],U_clip.T)[0] for i in range(nclip)]
-A = ma.vstack(A)
-
-fdt_stand_clip = fdt_stand[icolin]
-A_stand = [cotrend.bvfitm(fdt_stand_clip[i],U_clip.T)[0] for i in range(nclip)]
-A_stand = ma.vstack(A_stand)
-
-dftr_clip = dftr.iloc[icolin]
-
-kAs = ['A%02d' % i for i in range(n_components)]
-dfA = pd.DataFrame(A,columns=kAs,index=dftr_clip.index)
-dfA = pd.concat([dftr_clip,dfA],axis=1)
-
-# Label points as inliers
-dfAc = cotrend.dfA_get_coeffs(dfA)
-dfAc_st = pd.DataFrame(dfAc.median(),columns=['med'])
-dfAc_st['sig'] = 1.48*abs(dfAc - dfAc_st['med']).median()
-dfAc_st['outhi'] = dfAc_st['med'] + 5 * dfAc_st['sig']
-dfAc_st['outlo'] = dfAc_st['med'] - 5 * dfAc_st['sig']
-inlier = (dfAc_st.outlo < dfAc) & (dfAc < dfAc_st.outhi)
-dfA['inlier'] = np.all(inlier,axis=1)
-
-print ""
-print "%i stars in training set " % fdt.shape[0]
-print "%i stars survived iterative %s " % (fdt_clip.shape[0],algo)
-print "%i stars have inlier coefficients" % dfA.inlier.sum()
-
-clf()
-pd.scatter_matrix(dfA[dfA.inlier][kAs],figsize=(8,8))
-gcf().savefig(plot_basename+'_coeffs.png')
-
-clf()
-cotrend.plot_mode_FOV(dfA[dfA.inlier])
-gcf().set_tight_layout(True)
-gcf().savefig(plot_basename+'_FOV.png')
-
-path_train_dfA = path_train.replace('train','train-dfA')
-dfA.to_hdf(path_train_dfA,'dfA')
-dfAc_st.to_hdf(path_train_dfA,'dfAc_st')
-
-with h5plus.File(path_train) as h5:
-    for k in 'U V U_clip'.split():
-        exec "h5.create_dataset('%s',data=%s)" % (k,k)
+picklefn = dirname+'_ec.pickle'
+print "Saving calibrator object to %s" % picklefn
+with open(picklefn,'w') as f:
+    pickle.dump(ec,f)
