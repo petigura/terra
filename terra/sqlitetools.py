c0cee510d5aa93a5d038650a791a40c97411b30e
Erik Petigura <petigura@caltech.edu>
2016-01-12 09:39:02 -0800

    changes to summary stats

--- terra/sqlitetools.py
+++ terra/sqlitetools.py
@@ -1,25 +1,38 @@
 import os
 import sqlite3
 
-def create_table(dbfile, schemafile):
+
+def create_table(dbfile, schemafile, if_exists='pass'):
     """
     Checks if sqlite table exists. If not, creates it.
     """
-    if not os.path.isfile(dbfile):
+    database_exists = os.path.isfile(dbfile)
+    if database_exists and if_exists=='pass':
+        return
+
+    if database_exists and if_exists=='replace':
+        os.system('rm {}'.format(dbfile))
+        _create_table(dbfile, schemafile)
+        return
+
+    if not database_exists:
+        _create_table(dbfile, schemafile)
+        return 
+
+def _create_table(dbfile, schemafile):
+    with open(schemafile) as f:
+        schema = f.read()
+
+    con = sqlite3.connect(dbfile)
+    with con:
         print "creating {}".format(dbfile)
-        with open(schemafile) as f:
-            schema = f.read()
+        cur = con.cursor()
+        cur.execute(schema)
 
-        con = sqlite3.connect(dbfile)
-        with con:
-            cur = con.cursor()
-            cur.execute(schema)
-
-def insert_dict(d, table, dbfile, schemafile):
+def insert_dict(d, table, dbfile):
     """
-    Inserts dictionary into sqlite3 database. Also runs create_table.
+    Inserts dictionary into sqlite3 database
     """
-    create_table(dbfile, schemafile)
     columns = ', '.join(d.keys())
     placeholders = ':'+', :'.join(d.keys())
     sql = 'INSERT INTO {} ({}) VALUES ({})'.format(table, columns, placeholders)
